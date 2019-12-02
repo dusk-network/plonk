2f71dbda45aa7b1b60f68d86e080bba11078fb7c
decentralisedkev <kevtheappdev@gmail.com>
2019-12-02 20:47:17 +0000

    remove append_commitments from TranscriptProtocol

--- src/transcript.rs
+++ src/transcript.rs
@@ -7,9 +7,6 @@ pub trait TranscriptProtocol<E: PairingEngine> {
     /// Append a `commitment` with the given `label`.
     fn append_commitment(&mut self, label: &'static [u8], comm: &Commitment<E>);
 
-    /// Append `commitments` with the given `label`.
-    fn append_commitments(&mut self, labels: Vec<&'static [u8]>, comm: Vec<&Commitment<E>>);
-
     /// Compute a `label`ed challenge variable.
     fn challenge_scalar(&mut self, label: &'static [u8]) -> E::Fr;
 }
@@ -17,12 +14,6 @@ pub trait TranscriptProtocol<E: PairingEngine> {
 impl<E: PairingEngine> TranscriptProtocol<E> for Transcript {
     fn append_commitment(&mut self, label: &'static [u8], comm: &Commitment<E>) {
         self.append_message(label, &to_bytes![comm].unwrap());
-    }
-
-    fn append_commitments(&mut self, labels: Vec<&'static [u8]>, comms: Vec<&Commitment<E>>) {
-        for (comm, label) in comms.iter().zip(labels.iter()) {
-            self.append_message(label, &to_bytes![comm].unwrap());
-        }
     }
 
     fn challenge_scalar(&mut self, label: &'static [u8]) -> E::Fr {
