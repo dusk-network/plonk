60e871326f258ec702cf636078647d84f818929f
decentralisedkev <kevtheappdev@gmail.com>
2019-12-06 13:20:33 +0000

    use raw vector form instead of polynomial struct for preprocessed circuit sigma polynomials

--- src/cs/mod.rs
+++ src/cs/mod.rs
@@ -3,18 +3,20 @@ mod constraint_system;
 mod permutation;
 
 use algebra::curves::PairingEngine;
+use ff_fft::DensePolynomial as Polynomial;
 use poly_commit::kzg10::Commitment;
 
 use crate::transcript::TranscriptProtocol;
 use ff_fft::EvaluationDomain;
 use poly_commit::kzg10::UniversalParams;
+use rand_core::{CryptoRng, RngCore};
 
 // Preprocessed cirucit includes the commitment to the selector polynomials and the simga polynomials
 pub struct PreProcessedCircuit<E: PairingEngine> {
-    selector_polys: Vec<Commitment<E>>,
-    left_sigma_poly: Commitment<E>,
-    right_sigma_poly: Commitment<E>,
-    out_sigma_poly: Commitment<E>,
+    selector_polys: Vec<(Polynomial<E::Fr>, Commitment<E>)>,
+    left_sigma_poly: (Vec<E::Fr>, Commitment<E>),
+    right_sigma_poly: (Vec<E::Fr>, Commitment<E>),
+    out_sigma_poly: (Vec<E::Fr>, Commitment<E>),
 }
 
 // Empty proof struct that for now, will only be used for the Composer trait
@@ -34,9 +36,10 @@ pub trait Composer<E: PairingEngine> {
     // N.B. We could pass a `PreprocessedCircuit` into `Prove` however, we must ensure that it contains
     // enough state to build the rest of the proof. We can do this by adding the necessary polynomials into the
     // preprocessed circuit along with their commitments and size of circuit, etc
-    fn prove(
+    fn prove<R: RngCore + CryptoRng>(
         &mut self,
         public_parameters: &UniversalParams<E>,
         transcript: &mut dyn TranscriptProtocol<E>,
+        rng: &mut R,
     ) -> Proof;
 }
