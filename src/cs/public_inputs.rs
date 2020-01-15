5c9aaa0246518a771b1dcb94480eb13cf0364aa1
Luke Pearson <luke@dusk.network>
2020-01-15 18:24:12 +0100

    Add branch
    Adding branch public_inputs.rs
    to the repo

--- src/cs/public_inputs.rs
+++ src/cs/public_inputs.rs
@@ -9,7 +9,6 @@ use std::marker::PhantomData;
 
 /// Computation for the public inputs. Utilising the Lagrange polynomials, 
 /// and summing all of them bar the first. We also compute the sum of the witness 
-/// polynomials, which are notably seperate from the witnesses that are pruvate
-/// public inout is separate from witnessess that are private
+/// polynomials, which are notably seperate from the witnesses that are private.
 
 pub fn compute_langrange_polynomial
