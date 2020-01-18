9f259a01ac3c40c68e2e1a1a8780af895bf2a5fa
Carlos Pérez <carlos@dusk.network>
2020-01-18 19:33:37 +0100

    Implement Public Inputs poly generation
    Generates the Public Inputs polynomial PI(X) from
    a PreProcessedCircuit and the Public Input coeficients.
    
    Panics:
    If the degree of any of the selector polynomials is = 0.

--- src/cs/public_inputs.rs
+++ src/cs/public_inputs.rs
@@ -5,6 +5,7 @@ use algebra::{
     fields::{Field, PrimeField},
 };
 use ff_fft::{DensePolynomial as Polynomial, EvaluationDomain};
+use rayon::iter::*;
 use std::marker::PhantomData;
 
 /// Computation for the public inputs. Utilising the Lagrange polynomials,
@@ -19,5 +20,38 @@ impl<E: PairingEngine> PIInputsToolKit<E> {
         PIInputsToolKit {
             _engine: PhantomData,
         }
+    }
+
+    // Generates the Public Inputs polynomial PI(X) from
+    // a PreProcessedCircuit and the Public Input coeficients.
+    //
+    // # Panics
+    // If the degree of any of the selector polynomials is = 0.
+    pub fn gen_public_inputs_poly(
+        &self,
+        prep_circ: &PreProcessedCircuit<E>,
+        pub_inputs: &Vec<E::Fr>,
+    ) -> Polynomial<E::Fr> {
+        assert!(prep_circ.qm_poly().len() > 0);
+        // Get Lagrange polys.
+        let lagrange_polys = prep_circ.compute_n_lagrange_polys();
+        // Get PI negated factors as 0 degree polynomials
+        let neg_inputs_as_polys: Vec<Polynomial<E::Fr>> = pub_inputs
+            .into_iter()
+            .map(|field_elem| -*field_elem)
+            .map(|neg_field| Polynomial::from_coefficients_slice(&[neg_field]))
+            .collect();
+        // Compute `-x_i * L_i(X)` vector
+        let prod_poly: Vec<Polynomial<E::Fr>> = lagrange_polys
+            .into_par_iter()
+            .zip(neg_inputs_as_polys.into_par_iter())
+            .map(|(xi, li_x)| &xi * &li_x)
+            .collect();
+        // Sum up all of the product polys and return the result.
+        let mut res = Polynomial::zero();
+        for item in prod_poly {
+            res = &res + &item;
+        }
+        res
     }
 }
