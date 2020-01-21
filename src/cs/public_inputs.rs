8df9fbccaed73523779ff38bcc620deae3e76a8a
Luke Pearson <luke@dusk.network>
2020-01-21 20:17:56 +0100

    Add method for PI polynomial computation
    This function evaluates the coefficients for Lagrangian polynomials
    from inputted numbers, as vectors, across their domain.

--- src/cs/public_inputs.rs
+++ src/cs/public_inputs.rs
@@ -9,9 +9,9 @@ use rayon::iter::*;
 use std::marker::PhantomData;
 
 /// Computation for the public inputs. Utilising the Lagrange polynomials,
-/// and summing them all. For vectors of length n, where n is the number 
-/// of elements in the preselector polynomial vector. We also compute 
-/// the sum of the witness polynomials, which are notably seperate from 
+/// and summing them all. For vectors of length n, where n is the number
+/// of elements in the preselector polynomial vector. We also compute
+/// the sum of the witness polynomials, which are notably seperate from
 /// the witnesses that are private.
 
 pub struct PIInputsToolKit<E: PairingEngine> {
@@ -20,32 +20,14 @@ pub struct PIInputsToolKit<E: PairingEngine> {
 impl<E: PairingEngine> PIInputsToolKit<E> {
     pub fn new() -> Self {
         PIInputsToolKit {
-            _engine: PhantomData, 
+            _engine: PhantomData,
         }
     }
-
-
-    fn compute_public_inputs() {
-    use algebra::UniformRand;
-    let n = prep_circ.qm_poly().len();
-    let domain = EvaluationDomain::new(n).unwrap();
-    let w_i = vec![n];
-
-    let z = Fr::rand(&mut rand::thread_rng());
-    let l_coeffs = domain.evaluate_all_lagrange_coefficients(z);
-    assert_eq!(l_coeffs.len(), n);
-
-    let mut sum = Fr::zero();
-    for (x, y) in w_i.clone().into_iter().zip(l_coeffs) {
-        let partial_sum = x * &y;
-        sum = sum + &partial_sum;    
+    pub fn compute_pi_poly(w_i: &Vec<E::Fr>) -> Polynomial<E::Fr> {
+        let domain = EvaluationDomain::new(w_i.len()).unwrap();
+        Polynomial::from_coefficients_vec(domain.ifft(w_i))
     }
-
-    let w_i_poly = Polynomial::from_coefficients_vec(domain.ifft(&w_i));
-    let w_i_eval = w_i_poly.evaluate(z);
-    assert_eq!(w_i_eval, sum);
 }
-
 
 mod tests {
     use super::*;
