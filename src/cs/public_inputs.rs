b0c415859ff732cf10e57fc521a1883f977445a8
Carlos Pérez <carlos@dusk.network>
2020-01-22 17:36:16 +0100

    Add tests for PI poly computation and evaluation
    The tests were done by @decentralisedkev on a demonstration
    of the 2 ways of computing the poly in lagrange basis.

--- src/cs/public_inputs.rs
+++ src/cs/public_inputs.rs
@@ -23,11 +23,11 @@ impl<E: PairingEngine> PIInputsToolKit<E> {
             _engine: PhantomData,
         }
     }
-    pub fn compute_pi_poly(w_i: &Vec<E::Fr>) -> Polynomial<E::Fr> {
+    pub fn compute_pi_poly(&self, w_i: &Vec<E::Fr>) -> Polynomial<E::Fr> {
         let domain = EvaluationDomain::new(w_i.len()).unwrap();
         Polynomial::from_coefficients_vec(domain.ifft(w_i))
     }
-    pub fn evaluate_pi_poly(w_i: &Polynomial<E::Fr>, point: &E::Fr) -> E::Fr {
+    pub fn evaluate_pi_poly(&self, w_i: &Polynomial<E::Fr>, point: &E::Fr) -> E::Fr {
         w_i.evaluate(*point)
     }
 }
@@ -37,4 +37,27 @@ mod tests {
     use crate::cs::public_inputs::PIInputsToolKit;
     use algebra::curves::bls12_381::Bls12_381 as E;
     use algebra::fields::bls12_381::Fr;
+
+    #[test]
+    fn test_pi_poly_comp_eval() {
+        use algebra::UniformRand;
+
+        let n = 4;
+        let domain = EvaluationDomain::new(4).unwrap();
+        let w_i = vec![Fr::from(4u8), Fr::from(5u8), Fr::from(6u8), Fr::from(7u8)];
+
+        let z = Fr::rand(&mut rand::thread_rng());
+        let l_coeffs = domain.evaluate_all_lagrange_coefficients(z);
+        assert_eq!(l_coeffs.len(), 4);
+
+        let mut sum = Fr::zero();
+        for (x, y) in w_i.clone().into_iter().zip(l_coeffs) {
+            let partial_sum = x * &y;
+            sum = sum + &partial_sum;
+        }
+        let pi_toolkit: PIInputsToolKit<E> = PIInputsToolKit::new();
+        let p_i_x = pi_toolkit.compute_pi_poly(&w_i);
+        let p_i_z = pi_toolkit.evaluate_pi_poly(&p_i_x, &z);
+        assert_eq!(p_i_z, sum);
+    }
 }
