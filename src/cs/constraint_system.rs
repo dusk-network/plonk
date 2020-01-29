603ef298e5c0af930d5779bf3c2c056374a4135b
decentralisedkev <kevtheappdev@gmail.com>
2020-01-29 20:27:18 +0000

    - simplfies the WireData enum variant; removing the bit masks and shifting

--- src/cs/constraint_system.rs
+++ src/cs/constraint_system.rs
@@ -5,48 +5,10 @@ pub struct Variable(pub(super) usize);
 
 /// Stores the data for a specific wire in an arithmetic circuit
 /// This data is the gate index and the type of wire
-/// (1,Left) signifies that this wire belongs to the first gate and is the left wire
-pub(crate) struct WireData {
-    pub(super) gate_index: usize,
-    pub(super) wire_type: WireType,
-}
-
-impl WireData {
-    /// Creates a new WireData instance
-    pub fn new(index: usize, wire_type: WireType) -> Self {
-        WireData {
-            gate_index: index,
-            wire_type: wire_type,
-        }
-    }
-}
-
-const MASK: usize = (1 << 29) - 1;
-
-impl From<&usize> for WireData {
-    fn from(n: &usize) -> WireData {
-        let wire_idx = n & MASK;
-        WireData {
-            gate_index: wire_idx,
-            wire_type: n.into(),
-        }
-    }
-}
-/// Encoding for different wire types
-/// // XXX: Something like Right(10) would look like a better encoding
-#[derive(Copy, Clone)]
-pub enum WireType {
-    Left = 0,
-    Right = (1 << 30),
-    Output = (1 << 31),
-}
-
-impl From<&usize> for WireType {
-    fn from(n: &usize) -> WireType {
-        match ((n >> 30) as usize) & (3 as usize) {
-            2 => WireType::Output,
-            1 => WireType::Right,
-            _ => WireType::Left,
-        }
-    }
+/// Left(1) signifies that this wire belongs to the first gate and is the left wire
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+pub enum WireData {
+    Left(usize),
+    Right(usize),
+    Output(usize),
 }
