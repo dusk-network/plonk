ec04c52ff2d84ea2c1f64688abba130b6aba84ce
decentralisedkev <kev@dusk.network>
2020-03-02 17:34:28 +0000

    Removes Linear Combinations and Blinding Polynomial (#117)
    * Remove LinearCombinations and add helper methods to compute the output value
    
    * Remove unused import
    
    * Remove blinding polynomial from KZG10

--- src/commitment_scheme/kzg10/mod.rs
+++ src/commitment_scheme/kzg10/mod.rs
@@ -1,5 +1,4 @@
 use bls12_381::{G1Affine, G1Projective, Scalar};
-use rand_core::RngCore;
 // Code was taken and modified from Pratyush: https://github.com/scipr-lab/poly-commit/blob/master/src/kzg10/mod.rs
 pub mod errors;
 pub mod key;
@@ -31,36 +30,4 @@ impl Commitment {
     pub fn empty() -> Self {
         Commitment(G1Affine::identity())
     }
-}
-
-/// Blinding Polynomial is a random polynomial
-/// Which will be used to blind the original
-/// polynomial that we are committing to
-pub struct BlindingPolynomial(Vec<Scalar>);
-
-impl BlindingPolynomial {
-    // Initialise the zero polynomial
-    fn empty() -> Self {
-        BlindingPolynomial(vec![Scalar::zero(); 1])
-    }
-    /// Computes a random blinding polynomial
-    pub fn rand<R: RngCore>(hiding_degree: usize, mut rng: &mut R) -> Self {
-        // A polynomial with degree n has n+1 terms
-        let num_of_coeffs = hiding_degree + 1;
-        let rand_coeffs = compute_n_random_scalars(num_of_coeffs, &mut rng);
-        BlindingPolynomial(rand_coeffs)
-    }
-}
-
-fn compute_n_random_scalars<R: RngCore>(n: usize, rng: &mut R) -> Vec<Scalar> {
-    let mut vec = Vec::with_capacity(n);
-    for _ in 0..n {
-        vec.push(Scalar::from_raw([
-            rng.next_u64(),
-            rng.next_u64(),
-            rng.next_u64(),
-            rng.next_u64(),
-        ]))
-    }
-    vec
 }
--- src/commitment_scheme/mod.rs
+++ src/commitment_scheme/mod.rs
@@ -8,11 +8,4 @@ trait CommitmentScheme {
     type Proof;
 }
 
-/// N.B. KZG10 has two variations polycommit_DL and polycommit_PED
-/// PLONK uses polycommit_DL in the paper and blinds the polynomials directly
-/// However, doing it this way significantly increases the size of FFTs that are needed.
-/// This implementation will first use polycommit_DL, however we will allow users to switch to polycommit_PED
-/// This would increase the proof size by 3 elements and would require the a larger SRS in the ceremony
-/// XXX: In general, unless we can find a more economical way in regards to FFTs, it may be better to blind in the commitment scheme
-/// We note that the main FFT cost of 8n arise due to the blinded grand product polynomial component in the quotient polynomial
 pub mod kzg10;
