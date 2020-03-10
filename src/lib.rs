3716017b25a8af6e07594cb27ec27556bb5a60ed
Luke Pearson <luke@dusk.network>
2020-03-10 12:05:44 +0100

    Linking docs to crates.io

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(external_doc)]
 pub mod commitment_scheme;
 pub mod constraint_system;
 pub mod fft;
@@ -7,3 +8,15 @@ mod util;
 
 #[macro_use]
 extern crate failure;
+
+#[doc(include = "../docs/introduction.md")]
+mod notes {
+    #[doc(include = "../docs/notes-lp.md")]
+    mod notes_interpolation {}
+    #[doc(include = "../docs/notes-commitment.md")]
+    mod notes_KZG10 {}
+    #[doc(include = "../docs/notes-pp.md")]
+    mod notes_pa {}
+    #[doc(include = "../docs/notes-snark.md")]
+    mod notes_prove_verify {}
+}
