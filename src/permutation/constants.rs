f3d865811b33279297c7378cb6831c80be0434e1
Kevaundray Wedderburn <kev@dusk.network>
2020-03-16 19:41:00 +0000

    add a (supposed )coset generator for the fourth wire

--- src/permutation/constants.rs
+++ src/permutation/constants.rs
@@ -2,3 +2,4 @@ use bls12_381::Scalar;
 
 pub const K1: Scalar = Scalar::from_raw([7, 0, 0, 0]);
 pub const K2: Scalar = Scalar::from_raw([13, 0, 0, 0]);
+pub const K3: Scalar = Scalar::from_raw([17, 0, 0, 0]);
