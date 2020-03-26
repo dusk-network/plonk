06a138f3564355a2c7327f3cc6fa3b93664acc64
Kevaundray Wedderburn <kev@dusk.network>
2020-03-26 20:37:00 +0000

    Modify Preprocessed circuit to use Permutation widget

--- src/constraint_system/standard/preprocessed_circuit.rs
+++ src/constraint_system/standard/preprocessed_circuit.rs
@@ -1,6 +1,7 @@
-use crate::commitment_scheme::kzg10::Commitment;
-use crate::constraint_system::widget::{ArithmeticWidget, RangeWidget};
-use crate::fft::{Evaluations, Polynomial};
+use crate::constraint_system::widget::{
+    ArithmeticWidget, PermutationWidget, PublicInputWidget, RangeWidget,
+};
+use crate::fft::Evaluations;
 // Preprocessed circuit includes the commitment to the selector polynomials and the sigma polynomials
 // for the standard plonk composer
 pub struct PreProcessedCircuit {
@@ -9,12 +10,8 @@ pub struct PreProcessedCircuit {
 
     pub arithmetic: ArithmeticWidget,
     pub range: RangeWidget,
-
-    // Sigma polynomials and their commitments
-    pub left_sigma: (Polynomial, Commitment),
-    pub right_sigma: (Polynomial, Commitment),
-    pub out_sigma: (Polynomial, Commitment),
-    pub fourth_sigma: (Polynomial, Commitment),
+    pub permutation: PermutationWidget,
+    pub public_input: PublicInputWidget,
 
     // Preprocesses the 4n Evaluations for the vanishing polynomial, so they do not
     // need to be computed at the proving stage.
@@ -23,30 +20,6 @@ pub struct PreProcessedCircuit {
     pub(crate) v_h_coset_4n: Evaluations,
 }
 impl PreProcessedCircuit {
-    pub fn left_sigma_poly(&self) -> &Polynomial {
-        &self.left_sigma.0
-    }
-    pub fn right_sigma_poly(&self) -> &Polynomial {
-        &self.right_sigma.0
-    }
-    pub fn out_sigma_poly(&self) -> &Polynomial {
-        &self.out_sigma.0
-    }
-    pub fn fourth_sigma_poly(&self) -> &Polynomial {
-        &self.fourth_sigma.0
-    }
-    pub fn left_sigma_comm(&self) -> &Commitment {
-        &self.left_sigma.1
-    }
-    pub fn right_sigma_comm(&self) -> &Commitment {
-        &self.right_sigma.1
-    }
-    pub fn out_sigma_comm(&self) -> &Commitment {
-        &self.out_sigma.1
-    }
-    pub fn fourth_sigma_comm(&self) -> &Commitment {
-        &self.fourth_sigma.1
-    }
     pub fn v_h_coset_4n(&self) -> &Evaluations {
         &self.v_h_coset_4n
     }
