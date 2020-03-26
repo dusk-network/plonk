32b4bd133ca9bc5567c12d8517136de084e6b3e7
Kevaundray Wedderburn <kev@dusk.network>
2020-03-26 19:50:22 +0000

    Modify Preprocessed circuit to use widgets in place of the selector polynomials

--- src/constraint_system/standard/preprocessed_circuit.rs
+++ src/constraint_system/standard/preprocessed_circuit.rs
@@ -1,12 +1,14 @@
 use crate::commitment_scheme::kzg10::Commitment;
+use crate::constraint_system::widget::{ArithmeticWidget, RangeWidget};
 use crate::fft::{Evaluations, Polynomial};
 // Preprocessed circuit includes the commitment to the selector polynomials and the sigma polynomials
 // for the standard plonk composer
 pub struct PreProcessedCircuit {
     // The number of gates in the circuit
     pub n: usize,
-    // Selector polynomial coefficients , their commitments and their 4n evaluation points
-    pub selectors: Vec<(Polynomial, Commitment, Evaluations)>,
+
+    pub arithmetic: ArithmeticWidget,
+    pub range: RangeWidget,
 
     // Sigma polynomials and their commitments
     pub left_sigma: (Polynomial, Commitment),
@@ -21,30 +23,6 @@ pub struct PreProcessedCircuit {
     pub(crate) v_h_coset_4n: Evaluations,
 }
 impl PreProcessedCircuit {
-    pub fn qm_poly(&self) -> &Polynomial {
-        &self.selectors[0].0
-    }
-    pub fn ql_poly(&self) -> &Polynomial {
-        &self.selectors[1].0
-    }
-    pub fn qr_poly(&self) -> &Polynomial {
-        &self.selectors[2].0
-    }
-    pub fn qo_poly(&self) -> &Polynomial {
-        &self.selectors[3].0
-    }
-    pub fn qc_poly(&self) -> &Polynomial {
-        &self.selectors[4].0
-    }
-    pub fn q4_poly(&self) -> &Polynomial {
-        &self.selectors[5].0
-    }
-    pub fn qarith_poly(&self) -> &Polynomial {
-        &self.selectors[6].0
-    }
-    pub fn qrange_poly(&self) -> &Polynomial {
-        &self.selectors[7].0
-    }
     pub fn left_sigma_poly(&self) -> &Polynomial {
         &self.left_sigma.0
     }
@@ -57,30 +35,6 @@ impl PreProcessedCircuit {
     pub fn fourth_sigma_poly(&self) -> &Polynomial {
         &self.fourth_sigma.0
     }
-    pub fn qm_comm(&self) -> &Commitment {
-        &self.selectors[0].1
-    }
-    pub fn ql_comm(&self) -> &Commitment {
-        &self.selectors[1].1
-    }
-    pub fn qr_comm(&self) -> &Commitment {
-        &self.selectors[2].1
-    }
-    pub fn qo_comm(&self) -> &Commitment {
-        &self.selectors[3].1
-    }
-    pub fn qc_comm(&self) -> &Commitment {
-        &self.selectors[4].1
-    }
-    pub fn q4_comm(&self) -> &Commitment {
-        &self.selectors[5].1
-    }
-    pub fn qarith_comm(&self) -> &Commitment {
-        &self.selectors[6].1
-    }
-    pub fn qrange_comm(&self) -> &Commitment {
-        &self.selectors[7].1
-    }
     pub fn left_sigma_comm(&self) -> &Commitment {
         &self.left_sigma.1
     }
@@ -92,30 +46,6 @@ impl PreProcessedCircuit {
     }
     pub fn fourth_sigma_comm(&self) -> &Commitment {
         &self.fourth_sigma.1
-    }
-    pub fn qm_eval_4n(&self) -> &Evaluations {
-        &self.selectors[0].2
-    }
-    pub fn ql_eval_4n(&self) -> &Evaluations {
-        &self.selectors[1].2
-    }
-    pub fn qr_eval_4n(&self) -> &Evaluations {
-        &self.selectors[2].2
-    }
-    pub fn qo_eval_4n(&self) -> &Evaluations {
-        &self.selectors[3].2
-    }
-    pub fn qc_eval_4n(&self) -> &Evaluations {
-        &self.selectors[4].2
-    }
-    pub fn q4_eval_4n(&self) -> &Evaluations {
-        &self.selectors[5].2
-    }
-    pub fn qarith_eval_4n(&self) -> &Evaluations {
-        &self.selectors[6].2
-    }
-    pub fn qrange_eval_4n(&self) -> &Evaluations {
-        &self.selectors[7].2
     }
     pub fn v_h_coset_4n(&self) -> &Evaluations {
         &self.v_h_coset_4n
