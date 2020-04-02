f25c01b4000d5ad363122fe8f55f17a7f7a0db3d
CPerezz <c.perezbaro@gmail.com>
2020-04-02 14:10:25 +0200

    Add deny lints
    - No unsafe code (at least ATM)
    - Failure or err on intra_doc_link_resolution
    - Failure on non-existing `Debug` impl.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,11 @@
 #![allow(clippy::many_single_char_names)]
 // Bool expr are usually easier to read with match statements.
 #![allow(clippy::match_bool)]
+#![deny(intra_doc_link_resolution_failure)]
+#![deny(missing_debug_implementations)]
+//#![deny(missing_docs)]
+#![deny(unsafe_code)]
+
 mod bit_iterator;
 pub mod commitment_scheme;
 pub mod constraint_system;
