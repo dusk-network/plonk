749e0ebc171cbc66de5e471575c621498539370c
CPerezz <c.perezbaro@gmail.com>
2020-04-03 16:18:36 +0200

    Refactor lib.rs doc-comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
-//! Aztec's PLONK system implementation built by the [Dusk Network team](dusk.network)
+//! Permutations over Lagrange-bases for Oecumenical Noninteractive
+//! arguments of Knowledge (PLONK) is a zero knowledge proof system.
 //!
-//! This code has not been audited or reviewed so no security assumptions
-//! can be made over it.
+//! This protocol was created by:
+//! - Ariel Gabizon (Protocol Labs),
+//! - Zachary J. Williamson (Aztec Protocol)
+//! - Oana Ciobotaru
 //!
-//! USE AT YOUR OWN RISK.
+//! This crate contains a pure-rust implementation done by the [DuskNetwork team](dusk.network)
+//! of this algorithm using as a reference implementation this one done
+//! by the creators of the protocol:
+//! S
+//! https://github.com/AztecProtocol/barretenberg/blob/master/barretenberg/src/aztec/plonk/
+//!
 // Bitshift/Bitwise ops are allowed to gain performance.
 #![allow(clippy::suspicious_arithmetic_impl)]
 // Some structs do not have AddAssign or MulAssign impl.
