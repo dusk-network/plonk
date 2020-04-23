64af821988175fcad9f2d73be393ffd9eb6201d9
CPerezz <c.perezbaro@gmail.com>
2020-04-23 20:15:08 +0200

    Add badges to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,22 @@
 #![cfg_attr(feature = "nightly", feature(external_doc))]
+#![doc(
+    html_logo_url = "https://lh3.googleusercontent.com/SmwswGxtgIANTbDrCOn5EKcRBnVdHjmYsHYxLq2HZNXWCQ9-fZyaea-bNgdX9eR0XGSqiMFi=w128-h128-e365"
+)]
+#![doc(html_favicon_url = "https://dusk.network/lib/img/favicon-16x16.png")]
+//!<a href="https://codecov.io/gh/dusk-network/plonk">
+//!  <img src="https://codecov.io/gh/dusk-network/plonk/branch/master/graph/badge.svg" />
+//!</a>
+//! <a href="https://travis-ci.com/dusk-network/plonk">
+//! <img src="https://travis-ci.com/dusk-network/plonk.svg?branch=master" />
+//! </a>
+//! <a href="https://github.com/dusk-network/plonk">
+//! <img alt="GitHub issues" src="https://img.shields.io/github/issues-raw/dusk-network/plonk?style=plastic">
+//! </a>
+//! <a href="https://github.com/dusk-network/plonk/blob/master/LICENSE">
+//! <img alt="GitHub" src="https://img.shields.io/github/license/dusk-network/plonk?color=%230E55EF">
+//! </a>
+//!
+//!
 //! Permutations over Lagrange-bases for Oecumenical Noninteractive
 //! arguments of Knowledge (PLONK) is a zero knowledge proof system.
 //!
@@ -13,6 +31,8 @@
 //!
 //! https://github.com/AztecProtocol/barretenberg/blob/master/barretenberg/src/aztec/plonk/
 //!
+//! If you want to see library usage examples, please check:
+//! [https://github.com/dusk-network/plonk/tree/master/examples](https://github.com/dusk-network/plonk/tree/master/examples)
 // Bitshift/Bitwise ops are allowed to gain performance.
 #![allow(clippy::suspicious_arithmetic_impl)]
 // Some structs do not have AddAssign or MulAssign impl.
