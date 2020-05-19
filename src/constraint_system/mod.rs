b8854a5c0a7406e2194a9ba8538816daec46cc43
Kevaundray Wedderburn <kev@dusk.network>
2020-05-19 11:03:34 +0100

    Refactor constraint system to match new widget structure and split up the gates in the composer

--- src/constraint_system/mod.rs
+++ src/constraint_system/mod.rs
@@ -1,10 +1,18 @@
 //! The constraint System module stores the implementation
 //! of the PLONK Standard Composer, as well as the circuit
 //! tools and abstractions, used by the Composer to generate,
-//! build, preprocess & prove constructed circuits.
+//! build, preprocess circuits.
 pub(crate) mod composer;
 pub(crate) mod cs_errors;
 pub(crate) mod variable;
+
+// XXX : Put all of these in a composer folder
+pub(crate) mod arithmetic;
+pub(crate) mod boolean;
+#[cfg(test)]
+pub(crate) mod helper;
+pub(crate) mod logic;
+pub(crate) mod range;
 
 pub use composer::StandardComposer;
 pub use variable::{Variable, WireData};
