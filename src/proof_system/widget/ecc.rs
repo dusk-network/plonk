2b165675c94cfb3fdc772db5d23fc10cacdf1a47
Luke Pearson <luke@dusk.network>
2020-05-21 12:25:06 +0200

    Implement widget block
    The quotient contribution
    and the relevant delta functions
    have been implemented with this commit.

--- src/proof_system/widget/ecc.rs
+++ src/proof_system/widget/ecc.rs
@@ -116,138 +116,128 @@ impl ScalarMulWidget {
             q_ecc: PreProcessedPolynomial::new(q_ecc),
         }
     }
-}
-
-fn delta(f: Scalar) -> Scalar {
-    let f_1 = f - Scalar::one();
-    let f_2 = f - Scalar::from(2);
-    let f_3 = f - Scalar::from(3);
-    f * f_1 * f_2 * f_3
-}
-
-pub(crate) fn compute_quotient_i(
-    &self,
-    index: usize,
-    ecc_consistency_challenge: &Scalar,
-    w_l_i: &Scalar,
-    w_l_i_next: &Scalar,
-    w_r_i: &Scalar,
-    w_r_i_next: &Scalar,
-    w_o_i: &Scalar,
-    w_o_i_next: &Scalar,
-    w_4_i: &Scalar,
-    w_4_i_next: &Scalar,
-    q_o_i: &Scalar,
-    q_ecc_i: &Scalar,
-    q_1_i: &Scalar,
-    q_2_i: &Scalar
-) -> Scalar {
-
-    let q_ecc_i = &self.q_ecc.evaluations.as_ref().unwrap()[index];
-    let q_o_i = &self.q_o.evaluations.as_ref().unwrap()[index];
-    
-    let four = Scalar::from(4);
-    let nine = Scalar::from(9);
-    let one = Scalar::one();
-
-    let kappa = ecc_consistency_challenge.square();
-    let kappa_2 = kappa.square();
-    let kappa_3 = kappa_2 * kappa;
-    let kappa_4 = kappa_3 * kappa;
-    let kappa_5 = kappa_4 * kappa;
-    let kappa_6 = kappa_5 * kappa;
-    let kappa_7 = kappa_6 * kappa;
-    let kappa_8 = kappa_7 * kappa;
-    let kappa_9 = kappa_8 * kappa;
-
-    /// Compute the accumulator which tracks the current 
-    /// rounds scalar multiplier, which is depedent on the 
-    /// input bit
-    let acc_input = four * w_4_i;
-    let accum = w_4_i_next - acc_input;
-
-    let accum_sqr = accum.square();
-    
-    /// To compute the y-alpha, which is the y-coordinate that corresponds to the x which is added 
-    /// in each round then we use the formula below. This y-alpha is the y-coordianate that corresponds 
-    /// to the y of one of the two points in the look up table, or the y in their inverses. 
-    let a = w_o_i_next * q_o_i;
-    let b = a + q_ecc_i;
-    let y_alpha = b * accum;
-    
-
-    /// Check that the accumulator consistency at the identity element
-    /// (accum - 1)(accum - 3)(accum + 1)(accum + 3) = 0 
-    let a = accum_sqr - 9; 
-    let b = accum_sqr - 1;
-    let scalar_accum = a * b;
-    let c_1 = scalar_accum * kappa;
-
-
-    /// To compute x-alpha, which is the x-coordinate that we're adding in at each round. We need to
-    /// explicit formualae with selector polynomials based on the values given in the lookup table.
-    let a = accum_sqr * q_1_i;
-    let b = a + q_2_i;
-    let x_alpha_identity = b - w_o_i_next;
-    let w_1 = x_alpha_identity * kappa_2;
-    
-    /// Consistency check of the x_accumulator
-    let a = (w_l_i_next + w_l_i + w_o_i_next);
-    let b = (w_o_i_next - w_l_i);
-    let c = b.square();
-    let d = y_alpha - w_r_i;
-    let e = d.square();
-    let x_accumulator = (a + c) - e;
-    let a_1 = x_accumulator * kappa_3;
-
-    /// Consistency check of the y_accumulator;
-    let a = w_r_i_next - w_r_i;
-    let b = w_o_i_next - w_l_i;
-    let c = y_alpha - w_r_i;
-    let d = w_l_i - w_l_i_next;
-    let y_accumulator = (a + b) * (c + d);
-    let b_1 = y_accumulator * kappa_4;
-
-    /// Scalar accumulator consistency check;
-    let a = w_4_i - 1 - w_o_i;
-    let a = 
-
-    
-
-  
-    
-    // Check for consistency in the  x_value 
-
-    let x_alpha_minus_x_one = w_o_i_next - w_l_i; 
-
-    let 
-
-    // Check for consistency in the y_value 
-
-    let a_1 = 
-}
-
-
-
- 
-   
-
-
-
-    pub(crate) fn compute_linearisation(
+    pub(crate) fn compute_quotient_i(
         &self,
-        a_eval: &Scalar,
-        a_next_eval: &Scalar,
-        b_eval: &Scalar,
-        b_next_eval: &Scalar,
-        c_eval: &Scalar,
-        d_eval: &Scalar,
-        d_next_eval: &Scalar,
-        q_c_eval: &Scalar,
-    ) -> Polynomial (){}
-
+        index: usize,
+        ecc_consistency_challenge: &Fr,
+        w_l_i: &Fr,
+        w_l_i_next: &Fr,
+        w_r_i: &Fr,
+        w_r_i_next: &Fr,
+        w_o_i: &Fr,
+        w_o_i_next: &Fr,
+        w_4_i: &Fr,
+        w_4_i_next: &Fr,
+        q_5_i: &Fr,
+        q_m_i: &Fr,
+        q_4_i: &Fr
+        q_c_i: &Fr,
+    ) -> Fr {
+    
+        let q_ecc_i = &self.q_ecc.evaluations.as_ref().unwrap()[index];
+        let q_o_i = &self.q_o.evaluations.as_ref().unwrap()[index];
+        
+        let four = Scalar::from(4);
+        let nine = Scalar::from(9);
+        let one = Scalar::one();
+    
+        let kappa = ecc_consistency_challenge.square();
+        let kappa_2 = kappa.square();
+        let kappa_3 = kappa_2 * kappa;
+        let kappa_4 = kappa_3 * kappa;
+        let kappa_5 = kappa_4 * kappa;
+        let kappa_6 = kappa_5 * kappa;
+        let kappa_7 = kappa_6 * kappa;
+        let kappa_8 = kappa_7 * kappa;
+    
+        /// Compute the accumulator which tracks the current 
+        /// rounds scalar multiplier, which is depedent on the 
+        /// input bit
+        let acc_input = four * w_4_i;
+        let accum = w_4_i_next - acc_input;
+    
+        let accum_sqr = accum.square();
+        
+        /// To compute the y-alpha, which is the y-coordinate that corresponds to the x which is added 
+        /// in each round then we use the formula below. This y-alpha is the y-coordianate that corresponds 
+        /// to the y of one of the two points in the look up table, or the y in their inverses. 
+        let a = w_o_i_next * q_o_i;
+        let b = a + q_ecc_i;
+        let y_alpha = b * accum;
+        
+    
+        /// Check that the accumulator consistency at the identity element
+        /// (accum - 1)(accum - 3)(accum + 1)(accum + 3) = 0 
+        let a = accum_sqr - 9; 
+        let b = accum_sqr - 1;
+        let scalar_accum = a * b;
+        let c1 = scalar_accum * kappa;
+    
+    
+        /// To compute x-alpha, which is the x-coordinate that we're adding in at each round. We need to
+        /// explicit formualae with selector polynomials based on the values given in the lookup table.
+        let a = accum_sqr * q_1_i;
+        let b = a + q_2_i;
+        let x_alpha_identity = b - w_o_i_next;
+        let w1 = x_alpha_identity * kappa_2;
+        
+        /// Consistency check of the x_accumulator
+        let a = (w_l_i_next + w_l_i + w_o_i_next);
+        let b = (w_o_i_next - w_l_i);
+        let c = b.square();
+        let d = y_alpha - w_r_i;
+        let e = d.square();
+        let x_accumulator = (a + c) - e;
+        let a1 = x_accumulator * kappa_3;
+    
+        /// Consistency check of the y_accumulator;
+        let a = w_r_i_next - w_r_i;
+        let b = w_o_i_next - w_l_i;
+        let c = y_alpha - w_r_i;
+        let d = w_l_i - w_l_i_next;
+        let y_accumulator = (a + b) * (c + d);
+        let b1 = y_accumulator * kappa_4;
+    
+        /// Scalar accumulator consistency check;
+        let a = w_4_i - 1 - w_o_i;
+        let accum_init = a.square();
+        let c0 = accum_init * kappa_5;
+        
+        /// x_initial value consistency check;
+        let a = w_4_i - 1;
+        let b = (q_4_i - w_l_i) * w_o_i; 
+        let c = a * q_5_i;
+        let x_inital = b - c;
+        let a0 = x_inital * kappa_6;
+    
+        /// y_initial value consistency check;
+        let a = w_4_i - 1;
+        let b = (q_m_i - w_r_i) * w_o_i;
+        let c = a * q_c_i;
+        let y_initial = b - c;
+        let b0 = y_initial * kappa_7;
+    
+        let n = delta_ecc(&c0, &a0, &b0, &c1, &w1, &a1, &b1, &q_c) * kappa_8 * q_ecc_i;
+    }
+    
+    pub(crate) fn compute_linearisation(
+            &self,
+            a_eval: &Scalar,
+            a_next_eval: &Scalar,
+            b_eval: &Scalar,
+            b_next_eval: &Scalar,
+            c_eval: &Scalar,
+            d_eval: &Scalar,
+            d_next_eval: &Scalar,
+            q_c_eval: &Scalar,
+        ) -> Polynomial (){}
+    
+    
 
 }
+
+
+
 
 /// The polynomial identity to be evaluated, will check that the
 /// initialiser has been done correctly and check that the accumulating 
@@ -257,7 +247,7 @@ pub(crate) fn compute_quotient_i(
 /// A = B + C
 /// B = q_c * (c + let a + b)
 /// C = (c1 + w1 + a1 + b1)
-fn delta_ecc(c: &Fr, a: &Fr, b: &Fr, c_1: &Fr, w_1: &Fr, a_1: &Fr, b1_: &Fr, q_c: &Fr) -> {
+fn delta_ecc(c0: &Fr, a0: &Fr, b0: &Fr, c1: &Fr, w1: &Fr, a1: &Fr, b1: &Fr, q_c: &Fr) -> {
     
     let B = q_c * (c + a + b);
     let C = (c_1 + w_1 + a_1 + b_1);
