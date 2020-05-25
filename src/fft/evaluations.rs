95bc37c9008a012777878ecf811e84d19e3b5202
kevaundray <kev@dusk.network>
2020-05-25 12:29:42 +0100

    Temporarily removes serde (#249)

--- src/fft/evaluations.rs
+++ src/fft/evaluations.rs
@@ -4,8 +4,7 @@ use super::domain::EvaluationDomain;
 use super::polynomial::Polynomial;
 use core::ops::{Add, AddAssign, DivAssign, Index, Mul, MulAssign, Sub, SubAssign};
 use dusk_bls12_381::Scalar;
-#[cfg(feature = "serde")]
-use serde::{de::Visitor, ser::SerializeStruct, Deserialize, Deserializer, Serialize, Serializer};
+
 /// Stores a polynomial in evaluation form.
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct Evaluations {
@@ -15,90 +14,6 @@ pub struct Evaluations {
     domain: EvaluationDomain,
 }
 
-#[cfg(feature = "serde")]
-impl Serialize for Evaluations {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut eval_dom = serializer.serialize_struct("struct Evaluations", 2)?;
-        eval_dom.serialize_field("evals", &self.evals)?;
-        eval_dom.serialize_field("domain", &self.domain)?;
-        eval_dom.end()
-    }
-}
-
-#[cfg(feature = "serde")]
-impl<'de> Deserialize<'de> for Evaluations {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        enum Field {
-            Evals,
-            EvalDomain,
-        };
-
-        impl<'de> Deserialize<'de> for Field {
-            fn deserialize<D>(deserializer: D) -> Result<Field, D::Error>
-            where
-                D: Deserializer<'de>,
-            {
-                struct FieldVisitor;
-
-                impl<'de> Visitor<'de> for FieldVisitor {
-                    type Value = Field;
-
-                    fn expecting(
-                        &self,
-                        formatter: &mut ::core::fmt::Formatter,
-                    ) -> ::core::fmt::Result {
-                        formatter.write_str("struct Evaluations")
-                    }
-
-                    fn visit_str<E>(self, value: &str) -> Result<Field, E>
-                    where
-                        E: serde::de::Error,
-                    {
-                        match value {
-                            "evals" => Ok(Field::Evals),
-                            "domain" => Ok(Field::EvalDomain),
-                            _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
-                        }
-                    }
-                }
-
-                deserializer.deserialize_identifier(FieldVisitor)
-            }
-        }
-
-        struct EvaluationsVisitor;
-
-        impl<'de> Visitor<'de> for EvaluationsVisitor {
-            type Value = Evaluations;
-
-            fn expecting(&self, formatter: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
-                formatter.write_str("struct Evaluations")
-            }
-
-            fn visit_seq<V>(self, mut seq: V) -> Result<Evaluations, V::Error>
-            where
-                V: serde::de::SeqAccess<'de>,
-            {
-                let evals = seq
-                    .next_element()?
-                    .ok_or_else(|| serde::de::Error::invalid_length(0, &self))?;
-                let domain = seq
-                    .next_element()?
-                    .ok_or_else(|| serde::de::Error::invalid_length(0, &self))?;
-                Ok(Evaluations { evals, domain })
-            }
-        }
-
-        const FIELDS: &[&str] = &["evals", "domain"];
-        deserializer.deserialize_struct("Evaluations", FIELDS, EvaluationsVisitor)
-    }
-}
 impl Evaluations {
     /// Construct `Self` from evaluations and a domain.
     pub fn from_vec_and_domain(evals: Vec<Scalar>, domain: EvaluationDomain) -> Self {
@@ -200,32 +115,5 @@ impl<'a> DivAssign<&'a Evaluations> for Evaluations {
             .iter_mut()
             .zip(&other.evals)
             .for_each(|(a, b)| *a *= b.invert().unwrap());
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[cfg(feature = "serde")]
-    #[test]
-    fn evals_serde_roundtrip() {
-        use bincode;
-        let coeffs = vec![
-            Scalar::one(),
-            Scalar::one(),
-            Scalar::one(),
-            Scalar::one(),
-            Scalar::one(),
-            Scalar::one(),
-            Scalar::one(),
-            Scalar::one(),
-        ];
-        let dom = EvaluationDomain::new(coeffs.len()).unwrap();
-        let evals = Evaluations::from_vec_and_domain(coeffs, dom);
-        let ser = bincode::serialize(&evals).unwrap();
-        let deser: Evaluations = bincode::deserialize(&ser).unwrap();
-
-        assert_eq!(evals, deser);
     }
 }
