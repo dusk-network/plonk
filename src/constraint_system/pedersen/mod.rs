371e3889b9196e005c623d8ba94a578f50be18f2
Kevaundray Wedderburn <kev@dusk.network>
2020-07-08 18:44:53 +0100

    - Add Curve Addition functionality - Use BlsScalar predominantly and only use JubJubScalar for the wnaf calculation - Add test for point addition and Pedersen hash

--- src/constraint_system/pedersen/mod.rs
+++ src/constraint_system/pedersen/mod.rs
@@ -1,13 +1,12 @@
 /// ECC gate
 pub mod ecc;
-mod weistrass;
 
 use crate::constraint_system::variable::Variable;
 use crate::constraint_system::StandardComposer;
 use dusk_bls12_381::Scalar as BlsScalar;
 use ecc::WnafRound;
 use jubjub::Fr as JubJubScalar;
-use jubjub::{AffinePoint, ExtendedPoint, GENERATOR};
+use jubjub::{AffinePoint, ExtendedPoint};
 
 /// The result of a scalar multiplication
 #[derive(Debug)]
@@ -17,29 +16,174 @@ pub struct PointScalar {
     scalar: Variable,
 }
 
-fn compute_wnaf_point_multiples(generator: AffinePoint, num_bits: usize) -> Vec<AffinePoint> {
-    let mut multiples = vec![AffinePoint::default(); num_bits];
+fn compute_wnaf_point_multiples(generator: ExtendedPoint, num_bits: usize) -> Vec<AffinePoint> {
+    let mut multiples = vec![ExtendedPoint::default(); num_bits];
     multiples[0] = generator;
     for i in 1..num_bits {
-        let mut accumulator = ExtendedPoint::from(multiples[i - 1]);
-        accumulator = accumulator.double();
-
-        multiples[i] = (accumulator).into();
+        multiples[i] = multiples[i - 1].double();
     }
 
-    multiples
+    jubjub::batch_normalize(&mut multiples).collect()
+}
+
+use jubjub::Fq;
+fn edwards_d() -> Fq {
+    let num = Fq::from(10240);
+    let den = Fq::from(10241);
+    -(num * den.invert().unwrap())
+}
+
+/// Adds two curve points together
+pub fn curve_addition(
+    composer: &mut StandardComposer,
+    point_a: (Variable, Variable),
+    point_b: (Variable, Variable),
+) -> (Variable, Variable) {
+    let x1 = point_a.0;
+    let y1 = point_a.1;
+
+    let x2 = point_b.0;
+    let y2 = point_b.1;
+
+    // x1 * y2
+    let x1_y2 = composer.mul(
+        BlsScalar::one(),
+        x1,
+        y2,
+        BlsScalar::zero(),
+        BlsScalar::zero(),
+    );
+    // y1 * x2
+    let y1_x2 = composer.mul(
+        BlsScalar::one(),
+        y1,
+        x2,
+        BlsScalar::zero(),
+        BlsScalar::zero(),
+    );
+    // y1 * y2
+    let y1_y2 = composer.mul(
+        BlsScalar::one(),
+        y1,
+        y2,
+        BlsScalar::zero(),
+        BlsScalar::zero(),
+    );
+    // x1 * x2
+    let x1_x2 = composer.mul(
+        BlsScalar::one(),
+        x1,
+        x2,
+        BlsScalar::zero(),
+        BlsScalar::zero(),
+    );
+    // d x1x2 * y1y2
+    let d_x1_x2_y1_y2 = composer.mul(
+        edwards_d(),
+        x1_x2,
+        y1_y2,
+        BlsScalar::zero(),
+        BlsScalar::zero(),
+    );
+
+    // x1y2 + y1x2
+    let x_numerator = composer.add(
+        (BlsScalar::one(), x1_y2),
+        (BlsScalar::one(), y1_x2),
+        BlsScalar::zero(),
+        BlsScalar::zero(),
+    );
+
+    // y1y2 - a * x1x2 (a=-1) => y1y2 + x1x2
+    let y_numerator = composer.add(
+        (BlsScalar::one(), y1_y2),
+        (BlsScalar::one(), x1_x2),
+        BlsScalar::zero(),
+        BlsScalar::zero(),
+    );
+
+    // 1 + dx1x2y1y2
+    let x_denominator = composer.add(
+        (BlsScalar::one(), d_x1_x2_y1_y2),
+        (BlsScalar::zero(), composer.zero_var),
+        BlsScalar::one(),
+        BlsScalar::zero(),
+    );
+
+    // Compute the inverse
+    let inv_x_denom = composer
+        .variables
+        .get(&x_denominator)
+        .unwrap()
+        .invert()
+        .unwrap();
+    let inv_x_denom = composer.add_input(inv_x_denom);
+
+    // Assert that we actually have the inverse
+    // inv_x * x = 1
+    composer.mul_gate(
+        x_denominator,
+        inv_x_denom,
+        composer.zero_var,
+        BlsScalar::one(),
+        BlsScalar::zero(),
+        -BlsScalar::one(),
+        BlsScalar::zero(),
+    );
+
+    // 1 - dx1x2y1y2
+    let y_denominator = composer.add(
+        (-BlsScalar::one(), d_x1_x2_y1_y2),
+        (BlsScalar::zero(), composer.zero_var),
+        BlsScalar::one(),
+        BlsScalar::zero(),
+    );
+    let inv_y_denom = composer
+        .variables
+        .get(&y_denominator)
+        .unwrap()
+        .invert()
+        .unwrap();
+    let inv_y_denom = composer.add_input(inv_y_denom);
+    // Assert that we actually have the inverse
+    // inv_y * y = 1
+    composer.mul_gate(
+        y_denominator,
+        inv_y_denom,
+        composer.zero_var,
+        BlsScalar::one(),
+        BlsScalar::zero(),
+        -BlsScalar::one(),
+        BlsScalar::zero(),
+    );
+
+    // We can now use the inverses
+
+    let x_3 = composer.mul(
+        BlsScalar::one(),
+        inv_x_denom,
+        x_numerator,
+        BlsScalar::zero(),
+        BlsScalar::zero(),
+    );
+    let y_3 = composer.mul(
+        BlsScalar::one(),
+        inv_y_denom,
+        y_numerator,
+        BlsScalar::zero(),
+        BlsScalar::zero(),
+    );
+
+    (x_3, y_3)
 }
 
 /// Computes a Scalar multiplication with the input scalar and the fixed generator
-pub fn new_fixed_based_scalar_mul(
+pub fn fixed_based_scalar_mul(
     composer: &mut StandardComposer,
     jubjub_scalar: Variable,
+    generator: ExtendedPoint,
 ) -> PointScalar {
-    // First we get the generator
-    let generator = GENERATOR;
-    assert!(generator.is_prime_order().unwrap_u8() == 1);
-
-    // XXX: we can slice off 3 bits from the top, since F_r prime has 252 bits.
+    // XXX: we can slice off 3 bits from the top of wnaf, since F_r prime has 252 bits.
     // XXX :We can also move to base4 and have half the number of gates since wnaf adjacent entries product is zero, we will not go over the specified amount
     let num_bits = 256;
 
@@ -48,7 +192,7 @@ pub fn new_fixed_based_scalar_mul(
     point_multiples.reverse();
 
     // Fetch the raw scalar value as bls scalar, then convert to a jubjub scalar
-    // XXX: Not very Tidy
+    // XXX: Not very Tidy, impl From function in JubJub
     let raw_bls_scalar = composer.variables.get(&jubjub_scalar).unwrap();
     let raw_jubjub_scalar = JubJubScalar::from_bytes(&raw_bls_scalar.to_bytes()).unwrap();
 
@@ -57,8 +201,8 @@ pub fn new_fixed_based_scalar_mul(
     assert_eq!(wnaf_entries.len(), num_bits);
 
     // Initialise the accumulators
-    let mut scalar_acc: Vec<JubJubScalar> = Vec::new();
-    scalar_acc.push(JubJubScalar::zero());
+    let mut scalar_acc: Vec<BlsScalar> = Vec::new();
+    scalar_acc.push(BlsScalar::zero());
     let mut point_acc: Vec<AffinePoint> = Vec::new();
     point_acc.push(AffinePoint::identity());
 
@@ -69,13 +213,13 @@ pub fn new_fixed_based_scalar_mul(
     for (i, entry) in wnaf_entries.iter().rev().enumerate() {
         // Based on the WNAF, we decide what scalar and point to add
         let (scalar_to_add, point_to_add) = match entry {
-            0 => { (JubJubScalar::zero(), AffinePoint::identity())},
-            -1 => {(JubJubScalar::one().neg(), -point_multiples[i])},
-            1 => {(JubJubScalar::one(), point_multiples[i])},
+            0 => { (BlsScalar::zero(), AffinePoint::identity())},
+            -1 => {(BlsScalar::one().neg(), -point_multiples[i])},
+            1 => {(BlsScalar::one(), point_multiples[i])},
             _ => unreachable!("Currently WNAF_2(k) is supported. The possible values are 1, -1 and 0. Current entry is {}", entry),
         };
 
-        let prev_accumulator = JubJubScalar::from(2u64) * scalar_acc[i];
+        let prev_accumulator = BlsScalar::from(2u64) * scalar_acc[i];
         scalar_acc.push(prev_accumulator + scalar_to_add);
         point_acc
             .push((ExtendedPoint::from(point_acc[i]) + ExtendedPoint::from(point_to_add)).into());
@@ -90,11 +234,9 @@ pub fn new_fixed_based_scalar_mul(
         let acc_x = composer.add_input(point_acc[i].get_x());
         let acc_y = composer.add_input(point_acc[i].get_y());
 
-        // XXX: Not very Tidy. Convert from a JubJubScalar to a BlsScalar
-        let bls_scalar = BlsScalar::from_bytes(&scalar_acc[i].to_bytes()).unwrap();
-        let accumulated_bit = composer.add_input(bls_scalar);
+        let accumulated_bit = composer.add_input(scalar_acc[i]);
 
-        // We constraint the point accumulator to start from the Identity point
+        // We constrain the point accumulator to start from the Identity point
         // and the Scalar accumulator to start from zero
         if i == 0 {
             composer.constrain_to_constant(acc_x, BlsScalar::zero(), BlsScalar::zero());
@@ -124,14 +266,13 @@ pub fn new_fixed_based_scalar_mul(
     let acc_x = composer.add_input(point_acc[num_bits].get_x());
     let acc_y = composer.add_input(point_acc[num_bits].get_y());
     let xy_alpha = composer.zero_var;
-    let bls_scalar = BlsScalar::from_bytes(&scalar_acc[num_bits].to_bytes()).unwrap();
-    let accumulated_bit = composer.add_input(bls_scalar);
+    let last_accumulated_bit = composer.add_input(scalar_acc[num_bits]);
 
     composer.big_add_gate(
         acc_x,
         acc_y,
         xy_alpha,
-        Some(accumulated_bit),
+        Some(last_accumulated_bit),
         BlsScalar::zero(),
         BlsScalar::zero(),
         BlsScalar::zero(),
@@ -141,30 +282,12 @@ pub fn new_fixed_based_scalar_mul(
     );
 
     // Constrain the last element in the accumulator to be equal to the input jubjub scalar
-    composer.assert_equal(accumulated_bit, jubjub_scalar);
+    composer.assert_equal(last_accumulated_bit, jubjub_scalar);
 
     PointScalar {
         base_x: acc_x,
         base_y: acc_y,
-        scalar: accumulated_bit,
-    }
-}
-
-#[test]
-fn new_test_ladder_compute() {
-    let num_bits = 256;
-    let wnaf_point = compute_wnaf_point_multiples(GENERATOR, num_bits);
-
-    let generator = GENERATOR;
-
-    assert_eq!(wnaf_point[0], generator);
-    for i in 1..num_bits {
-        let point = wnaf_point[i];
-
-        let pow_two = JubJubScalar::from(2u64).pow(&[i as u64, 0, 0, 0]);
-        let expected_point: AffinePoint = (ExtendedPoint::from(generator) * pow_two).into();
-
-        assert_eq!(expected_point, point, "{}", i);
+        scalar: last_accumulated_bit,
     }
 }
 
@@ -172,6 +295,7 @@ fn new_test_ladder_compute() {
 mod tests {
     use super::super::helper::*;
     use super::*;
+    use jubjub::GENERATOR;
     #[test]
     fn test_ecc_constraint() {
         let res = gadget_tester(
@@ -186,13 +310,12 @@ mod tests {
                 let secret_scalar = composer.add_input(bls_scalar);
 
                 let expected_point: AffinePoint = (ExtendedPoint::from(GENERATOR) * scalar).into();
-                let expected_x = composer.add_input(expected_point.get_x());
-                let expected_y = composer.add_input(expected_point.get_y());
 
-                //
-                let point_scalar = new_fixed_based_scalar_mul(composer, secret_scalar);
-                composer.assert_equal(expected_x, point_scalar.base_x);
-                composer.assert_equal(expected_y, point_scalar.base_y);
+                let point_scalar =
+                    fixed_based_scalar_mul(composer, secret_scalar, GENERATOR.into());
+
+                composer
+                    .assert_equal_point((point_scalar.base_x, point_scalar.base_y), expected_point);
             },
             600,
         );
@@ -209,17 +332,15 @@ mod tests {
                 let secret_scalar = composer.add_input(bls_scalar);
 
                 let expected_point: AffinePoint = (ExtendedPoint::from(GENERATOR) * scalar).into();
-                let expected_x = composer.add_input(expected_point.get_x());
-                let expected_y = composer.add_input(expected_point.get_y());
 
-                //
-                let point_scalar = new_fixed_based_scalar_mul(composer, secret_scalar);
-                composer.assert_equal(expected_x, point_scalar.base_x);
-                composer.assert_equal(expected_y, point_scalar.base_y);
+                let point_scalar =
+                    fixed_based_scalar_mul(composer, secret_scalar, GENERATOR.into());
+
+                composer
+                    .assert_equal_point((point_scalar.base_x, point_scalar.base_y), expected_point);
             },
             600,
         );
-
         assert!(res.is_ok());
     }
     #[test]
@@ -237,8 +358,8 @@ mod tests {
                 let expected_x = composer.add_input(expected_point.get_x());
                 let expected_y = composer.add_input(expected_point.get_y());
 
-                //
-                let point_scalar = new_fixed_based_scalar_mul(composer, secret_scalar);
+                let point_scalar =
+                    fixed_based_scalar_mul(composer, secret_scalar, GENERATOR.into());
                 composer.assert_equal(expected_x, point_scalar.base_x);
                 composer.assert_equal(expected_y, point_scalar.base_y);
             },
@@ -246,5 +367,82 @@ mod tests {
         );
 
         assert!(res.is_err());
+    }
+    #[test]
+    fn test_point_addition() {
+        let res = gadget_tester(
+            |composer| {
+                let point_a = ExtendedPoint::from(GENERATOR);
+                let point_b = point_a.double();
+                let expected_point = point_a + point_b;
+
+                let affine_point_a: AffinePoint = point_a.into();
+                let affine_point_b: AffinePoint = point_b.into();
+                let affine_expected_point: AffinePoint = expected_point.into();
+
+                let var_point_a_x = composer.add_input(affine_point_a.get_x());
+                let var_point_a_y = composer.add_input(affine_point_a.get_y());
+                let var_point_b_x = composer.add_input(affine_point_b.get_x());
+                let var_point_b_y = composer.add_input(affine_point_b.get_y());
+
+                let new_point = curve_addition(
+                    composer,
+                    (var_point_a_x, var_point_a_y),
+                    (var_point_b_x, var_point_b_y),
+                );
+
+                composer.assert_equal_point(new_point, affine_expected_point);
+            },
+            600,
+        );
+
+        assert!(res.is_ok());
+    }
+    #[test]
+    #[allow(non_snake_case)]
+    fn test_pedersen_hash() {
+        let res = gadget_tester(
+            |composer| {
+                // First component
+                let scalar_a = JubJubScalar::from(112233u64);
+                let bls_scalar = BlsScalar::from_bytes(&scalar_a.to_bytes()).unwrap();
+                let secret_scalar_a = composer.add_input(bls_scalar);
+                let point_a = ExtendedPoint::from(GENERATOR);
+                let c_a: AffinePoint = (point_a * scalar_a).into();
+
+                // Second component
+                let scalar_b = JubJubScalar::from(445566u64);
+                let bls_scalar = BlsScalar::from_bytes(&scalar_b.to_bytes()).unwrap();
+                let secret_scalar_b = composer.add_input(bls_scalar);
+                let point_b = point_a.double() + point_a;
+                let c_b: AffinePoint = (point_b * scalar_b).into();
+
+                // Expected pedersen hash
+                let expected_point: AffinePoint = (point_a * scalar_a + point_b * scalar_b).into();
+
+                // To check this pedersen commitment, we will need to do:
+                // - Two scalar multiplications
+                // - One curve addition
+                //
+                // Scalar multiplications
+                let aG = fixed_based_scalar_mul(composer, secret_scalar_a, point_a);
+                let bH = fixed_based_scalar_mul(composer, secret_scalar_b, point_b);
+
+                // Depending on the context, one can check if the resulting aG and bH are as expected
+                //
+                composer.constrain_to_constant(aG.base_x, BlsScalar::zero(), -c_a.get_x());
+                composer.constrain_to_constant(aG.base_y, BlsScalar::zero(), -c_a.get_y());
+                composer.constrain_to_constant(bH.base_x, BlsScalar::zero(), -c_b.get_x());
+                composer.constrain_to_constant(bH.base_y, BlsScalar::zero(), -c_b.get_y());
+                // Curve addition
+                let commitment =
+                    curve_addition(composer, (aG.base_x, aG.base_y), (bH.base_x, bH.base_y));
+
+                // Add final constraints to ensure that the commitment that we computed is equal to the public point
+                composer.assert_equal_point(commitment, expected_point);
+            },
+            1024,
+        );
+        assert!(res.is_ok());
     }
 }
