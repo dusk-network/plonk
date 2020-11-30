9699acec6e08b612d7ba14857483a2b5154455b8
Luke Pearson <luke@dusk.network>
2020-11-30 23:53:22 +0100

    Add plook up gate
    A plookup gate, which assesses vector
    inputs is used to create plookup fns

--- src/constraint_system/composer.rs
+++ src/constraint_system/composer.rs
@@ -56,6 +56,8 @@ pub struct StandardComposer {
     pub(crate) q_fixed_group_add: Vec<BlsScalar>,
     // Variable base group addition selector
     pub(crate) q_variable_group_add: Vec<BlsScalar>,
+    // Plookup gate wire selector
+    // pub(crate) q_look_up: Vec<BlsScalar>,
 
     /// Public inputs vector
     pub public_inputs: Vec<BlsScalar>,
