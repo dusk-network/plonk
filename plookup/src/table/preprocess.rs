66dce17737581527102ca4605087b1ad2b446abf
Luke Pearson <luke@dusk.network>
2020-12-04 17:25:54 +0100

    Add multiset to witness table
    The witness table now consists
    of multisets of the wire values.

--- plookup/src/table/preprocess.rs
+++ plookup/src/table/preprocess.rs
@@ -4,31 +4,34 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-use crate::{kzg10, multiset::MultiSet};
-use algebra::{bls12_381::Fr, Bls12_381};
-use ff_fft::{DensePolynomial as Polynomial, EvaluationDomain};
-use poly_commit::kzg10::{Commitment, Powers};
+use crate::multiset::MultiSet;
+use crate::table::lookup_table::{PlookupTable3Arity, PlookupTable4Arity};
+use dusk_plonk::bls12_381::{BlsScalar, G1Affine};
+use dusk_plonk::commitment_scheme::kzg10::{AggregateProof, CommitKey, Commitment, OpeningKey};
+use dusk_plonk::fft::{EvaluationDomain, Polynomial};
 use std::collections::HashMap;
-use table::lookup_table::{PlookupTable3Arity, PlookupTable4Arity};
-/// This table will be the preprocessed version of the 
-/// precomputed table, T. This structure is passed to the 
-/// proof alongside the table of witness values. 
+/// This table will be the preprocessed version of the
+/// precomputed table, T. This structure is passed to the
+/// proof alongside the table of witness values.
 pub struct PreProcessedTableArity3 {
     pub n: usize,
-    pub t_1: (MultiSet, Commitment<Bls12_381>, Polynomial<Fr>),
-    pub t_2: (MultiSet, Commitment<Bls12_381>, Polynomial<Fr>),
-    pub t_3: (MultiSet, Commitment<Bls12_381>, Polynomial<Fr>),
+    pub t_1: (MultiSet, Commitment, Polynomial),
+    pub t_2: (MultiSet, Commitment, Polynomial),
+    pub t_3: (MultiSet, Commitment, Polynomial),
 }
 
-impl PreProcessedTableArity3 { 
-    fn preprocess(table: PlookupTable3Arity, commit_key: &Powers<Bls12_381>, n: usize) -> Self
-}
+// impl PreProcessedTableArity3 {
+//     fn preprocess(table: PlookupTable3Arity, commit_key: &CommitKey, n: usize) -> Self
+// }
+
 pub struct PreProcessedTableArity4 {
     pub n: usize,
-    pub t_1: (MultiSet, Commitment<Bls12_381>, Polynomial<Fr>),
-    pub t_2: (MultiSet, Commitment<Bls12_381>, Polynomial<Fr>),
-    pub t_3: (MultiSet, Commitment<Bls12_381>, Polynomial<Fr>),
-    pub t_4: (MultiSet, Commitment<Bls12_381>, Polynomial<Fr>),
+    pub t_1: (MultiSet, Commitment, Polynomial),
+    pub t_2: (MultiSet, Commitment, Polynomial),
+    pub t_3: (MultiSet, Commitment, Polynomial),
+    pub t_4: (MultiSet, Commitment, Polynomial),
 }
 
-
+// impl PreProcessedTableArity4 {
+//     fn preprocess(table: PlookupTable4Arity, commit_key: &CommitKey, n: usize) -> Self
+// }
