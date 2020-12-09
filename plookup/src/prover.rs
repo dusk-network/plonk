9f14b314352d24af84a85f6593387ff4457775d9
Luke Pearson <luke@dusk.network>
2020-12-09 22:56:39 +0100

    Build proof struct

--- plookup/src/prover.rs
+++ plookup/src/prover.rs
@@ -4,21 +4,40 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-use anyhow::{Error, Result};
-use dusk_plonk::commitment_scheme::kzg10;
 use crate::multiset::MultiSet;
 use crate::table::lookup_table::{PlookupTable3Arity, PlookupTable4Arity};
+use crate::table::preprocess::{PreprocessedTable3Arity, PreprocessedTable4Arity};
+use crate::table::witness_table::{WitnessTable3Arity, WitnessTable4Arity};
+use anyhow::{Error, Result};
 use dusk_plonk::bls12_381::{BlsScalar, G1Affine};
+use dusk_plonk::commitment_scheme::kzg10;
 use dusk_plonk::commitment_scheme::kzg10::{CommitKey, Commitment};
 use dusk_plonk::fft::{EvaluationDomain, Polynomial};
-use crate::table::witness_table::{WitnessTable3Arity, WitnessTable4Arity};
 
 pub struct PlookupProof3Arity {
     f: WitnessTable3Arity,
-    t: WitnessTable3Arity,
+    t: PreprocessedTable3Arity,
 }
 
 pub struct PlookupProof4Arity {
     f: WitnessTable4Arity,
-    t: WitnessTable4Arity,
+    t: PreprocessedTable4Arity,
+    proving_key: &Powers<bls12_381>,
+    transcript: &mut TranscriptProtocol,
+}
+
+impl PlookupProof4Arity {
+    pub fn create_proof(
+        f: WitnessTable4Arity,
+        t: PreprocessedTable4Arity,
+        proving_key: &Powers<bls12_381>,
+        transcript: &mut TranscriptProtocol,
+    ) -> PlookupProof4Arity {
+        let alpha = BlsScalar::from(15u64);
+        let f_set = compress_four_arity(f.0, alpha);
+        let t_set = compress_four_arity(t, alpha);
+
+        let s = t_set.sort_and_index(f_set);
+        let (h_1, h_2) = s.halve();
+    }
 }
