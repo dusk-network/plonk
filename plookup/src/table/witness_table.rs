11c7923a722a921676ca75d08d526b95742f8429
Luke Pearson <luke@dusk.network>
2020-12-09 22:58:28 +0100

    Interlink tables
    The tables are now using
    fucntions to read and auto
    complete one another. This
    allows for the auto creating
    seperate structures to make
    the proof clearer.

--- plookup/src/table/witness_table.rs
+++ plookup/src/table/witness_table.rs
@@ -4,8 +4,11 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
+use crate::error::PlookupErrors;
 use crate::multiset::MultiSet;
+use crate::table::lookup_table::{PlookupTable3Arity, PlookupTable4Arity};
 use dusk_plonk::bls12_381::BlsScalar;
+
 
 pub struct WitnessTable3Arity {
     pub f_1: MultiSet,
@@ -20,21 +23,71 @@ pub struct WitnessTable4Arity {
     pub f_4: MultiSet,
 }
 
-/*
-impl WitnessTable {
+impl WitnessTable3Arity {
+    /// This allows the witness table to be filled directly without
+    /// taking any vaules, or the the results, from the lookup table.
+    /// If the values do no exists in the lookup table, then the proof
+    /// will fail when witness and preprocessed tables are concatenated.
+    pub fn from_wire_values(
+        &mut self,
+        left_wire_val: BlsScalar,
+        right_wire_val: BlsScalar,
+        output_wire_val: BlsScalar,
+    ) {
+        self.f_1.push(left_wire_val);
+        self.f_2.push(right_wire_val);
+        self.f_3.push(output_wire_val);
+    }
 
-    pub from_wire_values(a: Vec<Variable>, b: Vec<Variable>, c: Vec<Variable>, d: Vec<Option<Vec>>) -> f: Vec![Variable] {
-
-        // Build a corresponding table out the a and b inputs of the
-        // same nature, to the one inputted.
-        let f_table = a
-            .iter()
-            .zip(b.iter())
-            .zip(c.iter())
-            .zip(d.iter())
-            .for_each(|(((left, right), output), fourth)| {
-                f_table.push(left);
-            });
+    /// Attempts to look up a value from a lookup table. If successful, all three
+    /// elements are pushed to their respective multisets.
+    pub fn value_from_table(
+        &mut self,
+        lookup_table: PlookupTable3Arity,
+        left_wire_val: BlsScalar,
+        right_wire_val: BlsScalar,
+    ) -> Result<(), PlookupErrors> {
+        let output_wire_val = lookup_table.lookup(left_wire_val, right_wire_val)?;
+        self.f_1.push(left_wire_val);
+        self.f_2.push(right_wire_val);
+        self.f_3.push(output_wire_val);
+        Ok(())
     }
 }
-*/
+
+impl WitnessTable4Arity {
+    /// This allows the witness table to be filled directly without
+    /// taking any vaules, or the the results, from the lookup table.
+    /// If the values do no exists in the lookup table, then the proof
+    /// will fail when witness and preprocessed tables are concatenated.
+    pub fn from_wire_values(
+        &mut self,
+        left_wire_val: BlsScalar,
+        right_wire_val: BlsScalar,
+        output_wire_val: BlsScalar,
+        fourth_wire_val: BlsScalar,
+    ) {
+        self.f_1.push(left_wire_val);
+        self.f_2.push(right_wire_val);
+        self.f_3.push(output_wire_val);
+        self.f_4.push(fourth_wire_val);
+    }
+
+    /// Attempts to look up a value from a lookup table. If successful, all four
+    /// elements are pushed to their respective multisets.
+    pub fn value_from_table(
+        &mut self,
+        lookup_table: PlookupTable4Arity,
+        left_wire_val: BlsScalar,
+        right_wire_val: BlsScalar,
+        fourth_wire_val: BlsScalar,
+    ) -> Result<(), PlookupErrors> {
+        let output_wire_val =
+            lookup_table.lookup(left_wire_val, right_wire_val, fourth_wire_val)?;
+        self.f_1.push(left_wire_val);
+        self.f_2.push(right_wire_val);
+        self.f_3.push(output_wire_val);
+        self.f_4.push(fourth_wire_val);
+        Ok(())
+    }
+}
