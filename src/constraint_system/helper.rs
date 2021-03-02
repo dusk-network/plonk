a6a987a8b45b1612377002bb1b311d7322bbfa4d
Joshua Fitzgerald <joshuabfitzgerald@gmail.com>
2021-03-02 14:42:19 -0500

    Adjust plookup gadget tester for new composer

--- src/constraint_system/helper.rs
+++ src/constraint_system/helper.rs
@@ -7,7 +7,7 @@
 use super::{PlookupComposer, StandardComposer};
 use crate::commitment_scheme::kzg10::PublicParameters;
 use crate::plookup::PreprocessedTable4Arity;
-use crate::proof_system::{Prover, Verifier};
+use crate::proof_system::{PlookupProver, PlookupVerifier, Prover, Verifier};
 use anyhow::{Error, Result};
 use dusk_bls12_381::BlsScalar;
 
@@ -98,56 +98,56 @@ pub(crate) fn gadget_tester(
     verifier.verify(&proof, &vk, &public_inputs)
 }
 
-// /// Takes a generic gadget function with no auxillary input and
-// /// tests whether it passes an end-to-end test
-// pub(crate) fn gadget_plookup_tester(
-//     gadget: fn(composer: &mut PlookupComposer),
-//     n: usize,
-//     lookup_table: PreprocessedTable4Arity,
-// ) -> Result<(), Error> {
-//     // Common View
-//     let public_parameters = PublicParameters::setup(2 * n, &mut rand::thread_rng())?;
-//     // Provers View
-//     let (proof, public_inputs) = {
-//         // Create a prover struct
-//         let mut prover = Prover::new(b"demo");
+/// Takes a generic gadget function with no auxillary input and
+/// tests whether it passes an end-to-end test
+pub(crate) fn gadget_plookup_tester(
+    gadget: fn(composer: &mut PlookupComposer),
+    n: usize,
+) -> Result<(), Error> {
+    // Common View
+    let public_parameters = PublicParameters::setup(2 * n, &mut rand::thread_rng())?;
+    // Provers View
+    let (proof, public_inputs, lookup_table) = {
+        // Create a prover struct
+        let mut prover = PlookupProver::new(b"demo");
 
-//         // Additionally key the transcript
-//         prover.key_transcript(b"key", b"additional seed information");
+        // Additionally key the transcript
+        prover.key_transcript(b"key", b"additional seed information");
 
-//         // Add gadgets
-//         gadget(&mut prover.mut_cs());
+        // Add gadgets
+        gadget(&mut prover.mut_cs());
 
-//         // Commit Key
-//         let (ck, _) = public_parameters.trim(2 * prover.cs.circuit_size().next_power_of_two())?;
+        // Commit Key
+        let (ck, _) = public_parameters.trim(2 * prover.cs.circuit_size().next_power_of_two())?;
 
-//         // Preprocess circuit
-//         prover.preprocess(&ck)?;
+        // Preprocess circuit
+        prover.preprocess(&ck)?;
 
-//         // Once the prove method is called, the public inputs are cleared
-//         // So pre-fetch these before calling Prove
-//         let public_inputs = prover.cs.public_inputs.clone();
+        // Once the prove method is called, the public inputs are cleared
+        // So pre-fetch these before calling Prove
+        let public_inputs = prover.cs.public_inputs.clone();
+        let lookup_table = prover.cs.lookup_table.clone();
 
-//         // Compute Proof
-//         (prover.prove(&ck)?, public_inputs)
-//     };
-//     // Verifiers view
-//     //
-//     // Create a Verifier object
-//     let mut verifier = PlookupVerifier::new(b"demo");
+        // Compute Proof
+        (prover.prove(&ck)?, public_inputs, lookup_table)
+    };
+    // Verifiers view
+    //
+    // Create a Verifier object
+    let mut verifier = PlookupVerifier::new(b"demo");
 
-//     // Additionally key the transcript
-//     verifier.key_transcript(b"key", b"additional seed information");
+    // Additionally key the transcript
+    verifier.key_transcript(b"key", b"additional seed information");
 
-//     // Add gadgets
-//     gadget(&mut verifier.mut_cs());
+    // Add gadgets
+    gadget(&mut verifier.mut_cs());
 
-//     // Compute Commit and Verifier Key
-//     let (ck, vk) = public_parameters.trim(verifier.cs.circuit_size().next_power_of_two())?;
+    // Compute Commit and Verifier Key
+    let (ck, vk) = public_parameters.trim(verifier.cs.circuit_size().next_power_of_two())?;
 
-//     // Preprocess circuit
-//     verifier.preprocess(&ck)?;
+    // Preprocess circuit
+    verifier.preprocess(&ck)?;
 
-//     // Verify proof
-//     verifier.verify(&proof, &vk, &public_inputs)
-// }
+    // Verify proof
+    verifier.verify(&proof, &vk, &public_inputs, &lookup_table)
+}
