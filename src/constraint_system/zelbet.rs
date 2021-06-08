a4e40913895ee225bd6dffcb22bd855dc1e9cc66
Errol Drummond <erroldrummond@protonmail.ch>
2021-06-08 14:49:44 +0200

    Updated zelbet function inputs
    Reinforced concrete hash has several functions
    that are used multiple times, and need to use
    variables relating to scalars representing one
    and two. Rather than adding gates for this, the
    variables can now be passed

--- src/constraint_system/zelbet.rs
+++ src/constraint_system/zelbet.rs
@@ -8,6 +8,8 @@
 
 use crate::constraint_system::StandardComposer;
 use crate::constraint_system::Variable;
+use crate::plookup::table::hash_tables::DECOMPOSITION_S_I;
+use crate::plookup::table::hash_tables::INVERSES_S_I;
 use crate::plookup::table::hash_tables::{BLS_SCALAR_REAL, SBOX_U256};
 use bigint::U256 as u256;
 use dusk_bls12_381::BlsScalar;
@@ -20,8 +22,7 @@ impl StandardComposer {
     pub fn decomposition_gadget(
         &mut self,
         x: Variable,
-        s_i: [BlsScalar; 27],
-        s_i_inv: [BlsScalar; 27],
+        s_i_decomposition: [Variable; 27],
     ) -> [Variable; 27] {
         let mut nibbles = [x; 27];
         // Reduced form needed for the modular operations
@@ -30,13 +31,11 @@ impl StandardComposer {
         let mut remainder = u256::zero();
 
         (0..27).for_each(|k| {
-            let s_ik = u256(s_i[k].0);
-
             match k < 26 {
                 true => {
-                    remainder = intermediate % s_ik;
+                    remainder = intermediate % u256(DECOMPOSITION_S_I[k].0);
                     let intermediate_scalar: BlsScalar =
-                        BlsScalar((intermediate - remainder).0) * s_i_inv[k];
+                        BlsScalar((intermediate - remainder).0) * INVERSES_S_I[k];
                     intermediate = u256(intermediate_scalar.0);
                 }
                 false => remainder = intermediate,
@@ -45,23 +44,21 @@ impl StandardComposer {
             nibbles[k] = self.add_input(BlsScalar::from_raw(remainder.0));
         });
 
-        let s_ik_var = self.add_input(BlsScalar::from_raw(s_i[25].0));
         // x' = x_1 * s_2 + x_2, this is the start of the composition
         let mut acc = self.big_mul(
             BlsScalar::one(),
             nibbles[26],
-            s_ik_var,
+            s_i_decomposition[25],
             Some((BlsScalar::one(), nibbles[25])),
             BlsScalar::zero(),
             BlsScalar::zero(),
         );
 
         (1..26).for_each(|k| {
-            let s_ik_var = self.add_input(BlsScalar::from_raw(s_i[25 - k].0));
             acc = self.big_mul(
                 BlsScalar::one(),
                 acc,
-                s_ik_var,
+                s_i_decomposition[25 - k],
                 Some((BlsScalar::one(), nibbles[25 - k])),
                 BlsScalar::zero(),
                 BlsScalar::zero(),
@@ -80,12 +77,14 @@ impl StandardComposer {
         input: Variable,
         counter: u64,
         conditional: bool,
+        one: Variable,
+        two: Variable,
     ) -> (Variable, Variable, bool, Variable) {
         // Need to convert input scalar value to non-Montgomery
         // to allow size comparison
         let value = self.variables[&input].reduce();
         let mut y_i = input;
-        let mut c_i = self.add_input(BlsScalar::one());
+        let mut c_i = one;
         let mut conditional_new = conditional;
         let mut z_i = self.zero_var;
         if value.0[0] < 659 {
@@ -93,13 +92,13 @@ impl StandardComposer {
             conditional_new = true;
         } else {
             y_i = input;
-            z_i = self.add_input(BlsScalar::one());
+            z_i = one;
             if value.0[0] > BLS_SCALAR_REAL[27 - counter as usize].0[0] {
-                c_i = self.add_input(BlsScalar::from(2));
+                c_i = two;
                 conditional_new = true
             } else if value.0[0] == BLS_SCALAR_REAL[27 - counter as usize].0[0] {
                 if conditional == true {
-                    c_i = self.add_input(BlsScalar::from(2));
+                    c_i = two;
                     conditional_new = true
                 } else {
                     c_i = self.zero_var
@@ -118,7 +117,7 @@ impl StandardComposer {
 mod tests {
     use super::super::helper::*;
     use super::*;
-    use crate::plookup::table::hash_tables::{DECOMPOSITION_S_I, INVERSES_S_I};
+    use crate::plookup::table::hash_tables::DECOMPOSITION_S_I;
     use crate::plookup::PlookupTable4Arity;
     use dusk_bls12_381::BlsScalar;
 
@@ -130,15 +129,19 @@ mod tests {
                 composer.append_lookup_table(&hash_table);
                 let seven_hundred = composer.add_input(BlsScalar::from(700));
                 let one = composer.add_input(BlsScalar::one());
+                let two = composer.add_input(BlsScalar::from(2));
                 let prime = composer.add_input(BlsScalar::from(659));
                 let counter: u64 = 1;
                 let counter2: u64 = 2;
                 let conditional = true;
                 let output_700 =
-                    composer.s_box_and_constraints(seven_hundred, counter2, conditional);
-                let output_one = composer.s_box_and_constraints(one, counter, conditional);
-                let output_prime = composer.s_box_and_constraints(prime, counter2, conditional);
-                let output_prime_false = composer.s_box_and_constraints(prime, counter, false);
+                    composer.s_box_and_constraints(seven_hundred, counter2, conditional, one, two);
+                let output_one =
+                    composer.s_box_and_constraints(one, counter, conditional, one, two);
+                let output_prime =
+                    composer.s_box_and_constraints(prime, counter2, conditional, one, two);
+                let output_prime_false =
+                    composer.s_box_and_constraints(prime, counter, false, one, two);
 
                 // Check that the s-box works as expected
                 composer.constrain_to_constant(
@@ -197,9 +200,10 @@ mod tests {
                 let hash_table = PlookupTable4Arity::create_hash_table();
                 composer.append_lookup_table(&hash_table);
                 let one = composer.add_input(BlsScalar::from(100));
+                let two = composer.add_input(BlsScalar::from(2));
                 let counter: u64 = 1;
                 let conditional = true;
-                let output = composer.s_box_and_constraints(one, counter, conditional);
+                let output = composer.s_box_and_constraints(one, counter, conditional, one, two);
                 composer.constrain_to_constant(
                     output.0,
                     BlsScalar::from_raw([200, 0, 0, 0]),
@@ -231,7 +235,11 @@ mod tests {
         let res = gadget_tester(
             |composer| {
                 let one = composer.add_input(BlsScalar::one());
-                let output = composer.decomposition_gadget(one, DECOMPOSITION_S_I, INVERSES_S_I);
+                let mut s_i_decomposition = [one; 27];
+                (0..27).for_each(|k| {
+                    s_i_decomposition[k] = composer.add_input(DECOMPOSITION_S_I[k]);
+                });
+                let output = composer.decomposition_gadget(one, s_i_decomposition);
                 (1..27).for_each(|k| {
                     composer.constrain_to_constant(output[k], BlsScalar::zero(), BlsScalar::zero());
                 });
