f9a777a6630642ac79ba804f2011092a7ea33fc9
Errol Drummond <erroldrummond@protonmail.ch>
2021-07-16 12:31:28 +0200

    License added to new file

--- src/constraint_system/divide_w_recip.rs
+++ src/constraint_system/divide_w_recip.rs
@@ -1,4 +1,8 @@
-//-----------------------------------------------------------------------------
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
 
 #[inline(always)]
 pub const fn full_shl(u: &[u64; 4], shift: u32) -> ([u64; 4], u64) {
