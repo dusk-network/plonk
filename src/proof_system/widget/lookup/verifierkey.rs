515d24964537c594ec8c40b0b854a23795743989
Victor Lopez <victor@dusk.network>
2021-09-23 21:40:54 +0200

    Add alloc feature for lookup Vec structs
    Resolves #582

--- src/proof_system/widget/lookup/verifierkey.rs
+++ src/proof_system/widget/lookup/verifierkey.rs
@@ -5,9 +5,6 @@
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
 use crate::commitment_scheme::kzg10::Commitment;
-use crate::proof_system::linearisation_poly::ProofEvaluations;
-use alloc::vec::Vec;
-use dusk_bls12_381::{BlsScalar, G1Affine};
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub struct VerifierKey {
@@ -18,72 +15,80 @@ pub struct VerifierKey {
     pub(crate) table_4: Commitment,
 }
 
-impl VerifierKey {
-    pub(crate) fn compute_linearisation_commitment(
-        &self,
-        lookup_separation_challenge: &BlsScalar,
-        scalars: &mut Vec<BlsScalar>,
-        points: &mut Vec<G1Affine>,
-        evaluations: &ProofEvaluations,
-        (delta, epsilon): (&BlsScalar, &BlsScalar),
-        zeta: &BlsScalar,
-        l1_eval: &BlsScalar,
-        t_eval: &BlsScalar,
-        t_next_eval: &BlsScalar,
-        h_2_comm: G1Affine,
-        p_comm: G1Affine,
-    ) {
-        let l_sep_2 = lookup_separation_challenge.square();
-        let l_sep_3 = lookup_separation_challenge * l_sep_2;
-        let zeta_sq = zeta * zeta;
-        let zeta_cu = zeta * zeta_sq;
+#[cfg(feature = "alloc")]
+mod alloc {
+    use super::VerifierKey;
+    use crate::proof_system::linearisation_poly::ProofEvaluations;
+    use alloc::vec::Vec;
+    use dusk_bls12_381::{BlsScalar, G1Affine};
 
-        // (a_eval + zeta*b_eval + zeta^2*c_eval + zeta^3d_eval - f_eval) *
-        // q_lookup * alpha_1
-        let a = {
-            let a_0 = evaluations.a_eval
-                + zeta * evaluations.b_eval
-                + zeta_sq * evaluations.c_eval
-                + zeta_cu * evaluations.d_eval;
-            let a_1 = evaluations.f_eval;
+    impl VerifierKey {
+        pub(crate) fn compute_linearisation_commitment(
+            &self,
+            lookup_separation_challenge: &BlsScalar,
+            scalars: &mut Vec<BlsScalar>,
+            points: &mut Vec<G1Affine>,
+            evaluations: &ProofEvaluations,
+            (delta, epsilon): (&BlsScalar, &BlsScalar),
+            zeta: &BlsScalar,
+            l1_eval: &BlsScalar,
+            t_eval: &BlsScalar,
+            t_next_eval: &BlsScalar,
+            h_2_comm: G1Affine,
+            p_comm: G1Affine,
+        ) {
+            let l_sep_2 = lookup_separation_challenge.square();
+            let l_sep_3 = lookup_separation_challenge * l_sep_2;
+            let zeta_sq = zeta * zeta;
+            let zeta_cu = zeta * zeta_sq;
 
-            (a_0 - a_1) * lookup_separation_challenge
-        };
+            // (a_eval + zeta*b_eval + zeta^2*c_eval + zeta^3d_eval - f_eval) *
+            // q_lookup * alpha_1
+            let a = {
+                let a_0 = evaluations.a_eval
+                    + zeta * evaluations.b_eval
+                    + zeta_sq * evaluations.c_eval
+                    + zeta_cu * evaluations.d_eval;
+                let a_1 = evaluations.f_eval;
 
-        scalars.push(a);
-        points.push(self.q_lookup.0);
+                (a_0 - a_1) * lookup_separation_challenge
+            };
 
-        //
-        // - (p_next_eval*(epsilon*(1 + delta) + h_1_eval +
-        //   delta*h_2_eval)*alpha_1^3)*h_2
-        let c = {
-            let c_0 = &evaluations.lookup_perm_eval;
+            scalars.push(a);
+            points.push(self.q_lookup.0);
 
-            let c_1 = epsilon * (BlsScalar::one() + delta)
-                + &evaluations.h_1_eval
-                + delta * &evaluations.h_2_eval;
+            //
+            // - (p_next_eval*(epsilon*(1 + delta) + h_1_eval +
+            //   delta*h_2_eval)*alpha_1^3)*h_2
+            let c = {
+                let c_0 = &evaluations.lookup_perm_eval;
 
-            -c_0 * c_1 * l_sep_3
-        };
-        scalars.push(c);
-        points.push(h_2_comm);
+                let c_1 = epsilon * (BlsScalar::one() + delta)
+                    + &evaluations.h_1_eval
+                    + delta * &evaluations.h_2_eval;
 
-        // (1 + delta)(e + f_eval)(epsilon*(1 + delta) + t_eval + (delta *
-        // t_next_eval) * alpha_1^3 + l_1(z) * alpha_1^2
-        let d = {
-            let d_0 = BlsScalar::one() + delta;
+                -c_0 * c_1 * l_sep_3
+            };
+            scalars.push(c);
+            points.push(h_2_comm);
 
-            let d_1 = epsilon + evaluations.f_eval;
+            // (1 + delta)(e + f_eval)(epsilon*(1 + delta) + t_eval + (delta *
+            // t_next_eval) * alpha_1^3 + l_1(z) * alpha_1^2
+            let d = {
+                let d_0 = BlsScalar::one() + delta;
 
-            let d_2 =
-                (epsilon * d_0 + t_eval + (delta * t_next_eval)) * l_sep_3;
+                let d_1 = epsilon + evaluations.f_eval;
 
-            let d_3 = l1_eval * l_sep_2;
+                let d_2 =
+                    (epsilon * d_0 + t_eval + (delta * t_next_eval)) * l_sep_3;
 
-            (d_0 * d_1 * d_2) + d_3
-        };
+                let d_3 = l1_eval * l_sep_2;
 
-        scalars.push(d);
-        points.push(p_comm);
+                (d_0 * d_1 * d_2) + d_3
+            };
+
+            scalars.push(d);
+            points.push(p_comm);
+        }
     }
 }
