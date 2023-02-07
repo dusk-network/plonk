4a30fab28eeb2b850f9bc9cff6da19799f0e7123
moana <moana@dusk.network>
2023-02-07 18:00:51 +0100

    Add common test function `check_satisfied_circuit_fails`

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -42,10 +42,38 @@ pub(crate) fn check_satisfied_circuit<C, R>(
 
     assert_eq!(*pi_expected, pi_circuit);
 
-    verifier.verify(&proof, &pi_circuit).expect(msg);
+    verifier.verify(&proof, &pi_expected).expect(msg);
+}
+
+// Check that proof creation and verification of a satisfied circuit fails
+// when the public inputs from the test circuit does not match the ones from
+// the verifier circuit description
+// As this is a very specific test case, this function will not be used by all
+// tests.
+#[allow(dead_code)]
+pub(crate) fn check_satisfied_circuit_fails<C, R>(
+    prover: &Prover<C>,
+    verifier: &Verifier<C>,
+    pi_expected: &Vec<BlsScalar>,
+    circuit: &C,
+    rng: &mut R,
+    msg: &str,
+) where
+    C: Circuit,
+    R: RngCore + CryptoRng,
+{
+    let (proof, pi_circuit) = prover
+        .prove(rng, circuit)
+        .expect("Prover for valid circuit shouldn't fail");
+
+    assert_eq!(*pi_expected, pi_circuit);
+
+    verifier.verify(&proof, &pi_expected).expect_err(msg);
 }
 
 // Check that proof creation of an unsatisfied circuit fails
+// This is also the case when the constants appended to the circuit does not
+// match the ones from the circuit description
 pub(crate) fn check_unsatisfied_circuit<C, R>(
     prover: &Prover<C>,
     circuit: &C,
