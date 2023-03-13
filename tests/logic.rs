8843fc610178e2729f384cabf6a083ddcd33f56e
moana <moana@dusk.network>
2023-03-13 16:16:40 +0100

    Add and refactor tests for logic gates
    Resolves #734

--- tests/logic.rs
+++ tests/logic.rs
@@ -8,310 +8,399 @@ use dusk_plonk::prelude::*;
 use rand::rngs::StdRng;
 use rand::SeedableRng;
 
+mod common;
+use common::{check_satisfied_circuit, check_unsatisfied_circuit, setup};
+
 #[test]
-fn logic_and_works() {
-    let rng = &mut StdRng::seed_from_u64(8349u64);
-
-    let n = 1 << 8;
-    let label = b"demo";
-    let pp = PublicParameters::setup(n, rng).expect("failed to create pp");
-
-    pub struct DummyCircuit {
+fn append_logic_and() {
+    #[derive(Default)]
+    pub struct TestCircuit {
         a: BlsScalar,
         b: BlsScalar,
-        c: BlsScalar,
+        result: BlsScalar,
         bits: usize,
     }
 
-    impl DummyCircuit {
+    impl TestCircuit {
         pub fn new(a: BlsScalar, b: BlsScalar, bits: usize) -> Self {
-            let x = BlsScalar::pow_of_2(bits as u64) - BlsScalar::one();
+            let bit_mask = BlsScalar::pow_of_2(bits as u64) - BlsScalar::one();
 
-            let a = a & x;
-            let b = b & x;
-            let c = a & b & x;
+            // BlsScalar are max 255 bits long so a bit_mask with more than 255
+            // bits will be overflowing and therefore incorrect
+            let result = match bits < 256 {
+                true => a & b & bit_mask,
+                false => a & b,
+            };
 
-            Self { a, b, c, bits }
+            Self { a, b, result, bits }
         }
     }
 
-    impl Default for DummyCircuit {
-        fn default() -> Self {
-            Self::new(7u64.into(), 8u64.into(), 256)
-        }
-    }
-
-    impl Circuit for DummyCircuit {
+    impl Circuit for TestCircuit {
         fn circuit<C>(&self, composer: &mut C) -> Result<(), Error>
         where
             C: Composer,
         {
             let w_a = composer.append_witness(self.a);
             let w_b = composer.append_witness(self.b);
-            let w_c = composer.append_witness(self.c);
+            let w_result = composer.append_witness(self.result);
 
-            let w_x = composer.append_logic_and(w_a, w_b, self.bits);
+            let circuit_result = composer.append_logic_and(w_a, w_b, self.bits);
 
-            composer.assert_equal(w_c, w_x);
+            composer.assert_equal(w_result, circuit_result);
 
             Ok(())
         }
     }
 
-    let (prover, verifier) = Compiler::compile::<DummyCircuit>(&pp, label)
-        .expect("failed to compile circuit");
+    // Compile common circuit descriptions for the prover and verifier with the
+    // default circuit
+    let label = b"append_logic_and";
+    let rng = &mut StdRng::seed_from_u64(0x1ead);
+    let capacity = 1 << 8;
+    let (prover, verifier) =
+        setup(capacity, rng, label, &TestCircuit::default());
 
-    // default works
-    {
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
+    // Common public input vector to be used by all tests
+    let pi = vec![];
 
-        let (proof, public_inputs) = prover
-            .prove(rng, &DummyCircuit::new(a, b, 256))
-            .expect("failed to prove");
+    // Test with bits = 0
+    //
+    // Test default works
+    let msg = "Default circuit verification should pass";
+    let circuit = TestCircuit::default();
+    check_satisfied_circuit(&prover, &verifier, &pi, &circuit, rng, &msg);
 
-        verifier
-            .verify(&proof, &public_inputs)
-            .expect("failed to verify proof");
-    }
+    // Test comparing 0 bits is always zero
+    let msg = "Circuit verification of satisfied circuit should pass";
+    let a = BlsScalar::random(rng);
+    let b = BlsScalar::random(rng);
+    let circuit = TestCircuit {
+        a,
+        b,
+        result: BlsScalar::zero(),
+        bits: 0,
+    };
+    check_satisfied_circuit(&prover, &verifier, &pi, &circuit, rng, &msg);
 
-    // negative works
-    {
-        let bits = 256;
+    // Test with bits = 32
+    //
+    // Create new prover and verifier circuit descriptions
+    let bits = 32;
+    let a = BlsScalar::zero();
+    let b = BlsScalar::zero();
+    let circuit = TestCircuit::new(a, b, bits);
+    let (prover, verifier) = setup(capacity, rng, label, &circuit);
 
-        let x = BlsScalar::pow_of_2(bits as u64) - BlsScalar::one();
+    // Test sanity:
+    let a = BlsScalar::from(0x0f0f_0ff0_0f0f_0ff0);
+    let b = BlsScalar::from(0xffff_0000_0000_ffff);
+    let result = BlsScalar::from(0x0000_0ff0);
+    let circuit = TestCircuit { a, b, result, bits };
+    check_satisfied_circuit(&prover, &verifier, &pi, &circuit, rng, &msg);
 
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
+    // Test random works:
+    let a = BlsScalar::random(rng);
+    let b = BlsScalar::random(rng);
+    let circuit = TestCircuit::new(a, b, bits);
+    check_satisfied_circuit(&prover, &verifier, &pi, &circuit, rng, &msg);
 
-        let a = a & x;
-        let b = b & x;
-        let c = a & b & x;
+    // Test invalid circuit fails
+    let msg = "Proof creation of unsatisfied circuit should fail";
+    let bit_mask = BlsScalar::pow_of_2(bits as u64) - BlsScalar::one();
+    let a = BlsScalar::random(rng);
+    let b = BlsScalar::random(rng);
+    let right_result = a & b & bit_mask;
+    let c = BlsScalar::random(rng);
+    let wrong_result = a & c & bit_mask;
+    assert_ne!(right_result, wrong_result);
+    let circuit_unsatisfied = TestCircuit {
+        a,
+        b,
+        result: wrong_result,
+        bits,
+    };
+    check_unsatisfied_circuit(&prover, &circuit_unsatisfied, rng, &msg);
+    // sanity check
+    let circuit_satisfied = TestCircuit {
+        a,
+        b,
+        result: right_result,
+        bits,
+    };
+    check_satisfied_circuit(
+        &prover,
+        &verifier,
+        &pi,
+        &circuit_satisfied,
+        rng,
+        &"Sanity check should pass",
+    );
 
-        let m = BlsScalar::random(rng) & x;
-        let n = a & m & x;
+    // Test with bits = 256
+    //
+    // Create new circuit description for the prover and verifier
+    let bits = 256;
+    let a = BlsScalar::zero();
+    let b = BlsScalar::zero();
+    let circuit = TestCircuit::new(a, b, bits);
+    let (prover, verifier) = setup(capacity, rng, label, &circuit);
 
-        assert_ne!(c, n);
+    // Test sanity:
+    let a = -BlsScalar::one();
+    let b = -BlsScalar::one();
+    let result = -BlsScalar::one();
+    let circuit = TestCircuit { a, b, result, bits };
+    check_satisfied_circuit(&prover, &verifier, &pi, &circuit, rng, &msg);
 
-        prover
-            .prove(rng, &DummyCircuit { a, b, c: n, bits })
-            .expect_err("the provided proof isn't valid");
-    }
+    // Test random works:
+    let msg = "Circuit verification with random values should pass";
+    let a = BlsScalar::random(rng);
+    let b = BlsScalar::random(rng);
+    let circuit = TestCircuit::new(a, b, bits);
+    check_satisfied_circuit(&prover, &verifier, &pi, &circuit, rng, &msg);
 
-    // small bits works
-    {
-        let bits = 30;
+    // Test invalid circuit fails
+    let msg = "Proof creation of unsatisfied circuit should fail";
+    let a = BlsScalar::random(rng);
+    let b = BlsScalar::random(rng);
+    let right_result = a & b;
+    let c = BlsScalar::random(rng);
+    let wrong_result = a & c;
+    assert_ne!(right_result, wrong_result);
+    let circuit = TestCircuit {
+        a,
+        b,
+        result: wrong_result,
+        bits,
+    };
+    check_unsatisfied_circuit(&prover, &circuit, rng, &msg);
+    // sanity check
+    let circuit_satisfied = TestCircuit {
+        a,
+        b,
+        result: right_result,
+        bits,
+    };
+    check_satisfied_circuit(
+        &prover,
+        &verifier,
+        &pi,
+        &circuit_satisfied,
+        rng,
+        &"Sanity check should pass",
+    );
 
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
+    // Test with odd bits = 55
+    //
+    // Create new prover and verifier circuit descriptions
+    let bits = 55;
+    let a = BlsScalar::zero();
+    let b = BlsScalar::zero();
+    let circuit = TestCircuit::new(a, b, bits);
+    let (prover, _) = setup(capacity, rng, label, &circuit);
 
-        let circuit = DummyCircuit::new(a, b, bits);
-
-        let (prover, verifier) =
-            Compiler::compile_with_circuit(&pp, label, &circuit)
-                .expect("failed to compile circuit");
-
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
-
-        let (proof, public_inputs) = prover
-            .prove(rng, &DummyCircuit::new(a, b, bits))
-            .expect("failed to prove");
-
-        verifier
-            .verify(&proof, &public_inputs)
-            .expect("failed to verify proof");
-    }
-
-    // zero bits works
-    {
-        let bits = 0;
-
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
-
-        let circuit = DummyCircuit::new(a, b, bits);
-
-        let (prover, verifier) =
-            Compiler::compile_with_circuit(&pp, label, &circuit)
-                .expect("failed to compile circuit");
-
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
-
-        let (proof, public_inputs) = prover
-            .prove(rng, &DummyCircuit::new(a, b, bits))
-            .expect("failed to prove");
-
-        verifier
-            .verify(&proof, &public_inputs)
-            .expect("failed to verify proof");
-    }
-
-    // odd bits will compile
-    {
-        let bits = 55;
-
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
-
-        let circuit = DummyCircuit::new(a, b, bits);
-
-        Compiler::compile_with_circuit(&pp, label, &circuit)
-            .expect("failed to compile circuit");
-    }
+    // Test random fails:
+    let msg = "Circuit verification with odd bits should fail";
+    let a = BlsScalar::random(rng);
+    let b = BlsScalar::random(rng);
+    let circuit = TestCircuit::new(a, b, bits);
+    check_unsatisfied_circuit(&prover, &circuit, rng, &msg);
 }
 
 #[test]
-fn logic_xor_works() {
-    let rng = &mut StdRng::seed_from_u64(8349u64);
-
-    let n = 1 << 8;
-    let label = b"demo";
-    let pp = PublicParameters::setup(n, rng).expect("failed to create pp");
-
-    pub struct DummyCircuit {
+fn append_logic_xor() {
+    #[derive(Default)]
+    pub struct TestCircuit {
         a: BlsScalar,
         b: BlsScalar,
-        c: BlsScalar,
+        result: BlsScalar,
         bits: usize,
     }
 
-    impl DummyCircuit {
+    impl TestCircuit {
         pub fn new(a: BlsScalar, b: BlsScalar, bits: usize) -> Self {
-            let x = BlsScalar::pow_of_2(bits as u64) - BlsScalar::one();
+            let bit_mask = BlsScalar::pow_of_2(bits as u64) - BlsScalar::one();
 
-            let a = a & x;
-            let b = b & x;
-            let c = (a ^ b) & x;
+            // BlsScalar are max 255 bits long so a bit_mask with more than 255
+            // bits will be overflowing and incorrect
+            let result = match bits < 256 {
+                true => (a ^ b) & bit_mask,
+                false => a ^ b,
+            };
 
-            Self { a, b, c, bits }
+            Self { a, b, result, bits }
         }
     }
 
-    impl Default for DummyCircuit {
-        fn default() -> Self {
-            Self::new(7u64.into(), 8u64.into(), 256)
-        }
-    }
-
-    impl Circuit for DummyCircuit {
+    impl Circuit for TestCircuit {
         fn circuit<C>(&self, composer: &mut C) -> Result<(), Error>
         where
             C: Composer,
         {
             let w_a = composer.append_witness(self.a);
             let w_b = composer.append_witness(self.b);
-            let w_c = composer.append_witness(self.c);
+            let w_result = composer.append_witness(self.result);
 
-            let w_x = composer.append_logic_xor(w_a, w_b, self.bits);
+            let circuit_result = composer.append_logic_xor(w_a, w_b, self.bits);
 
-            composer.assert_equal(w_c, w_x);
+            composer.assert_equal(w_result, circuit_result);
 
             Ok(())
         }
     }
 
-    let (prover, verifier) = Compiler::compile::<DummyCircuit>(&pp, label)
-        .expect("failed to compile circuit");
+    // Compile common circuit descriptions for the prover and verifier to be
+    // used by all tests
+    let label = b"append_logic_xor";
+    let rng = &mut StdRng::seed_from_u64(0xdea1);
+    let capacity = 1 << 8;
+    let (prover, verifier) =
+        setup(capacity, rng, label, &TestCircuit::default());
 
-    // default works
-    {
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
+    // Common values to be used by all tests
+    let pi = vec![];
 
-        let (proof, public_inputs) = prover
-            .prove(rng, &DummyCircuit::new(a, b, 256))
-            .expect("failed to prove");
+    // Test with bits = 0
+    //
+    // Test default works
+    let msg = "Default circuit verification should pass";
+    let circuit = TestCircuit::default();
+    check_satisfied_circuit(&prover, &verifier, &pi, &circuit, rng, &msg);
 
-        verifier
-            .verify(&proof, &public_inputs)
-            .expect("failed to verify proof");
-    }
+    // Test comparing 0 bits is always zero
+    let msg = "Circuit verification of satisfied circuit should pass";
+    let a = BlsScalar::random(rng);
+    let b = BlsScalar::random(rng);
+    let circuit = TestCircuit {
+        a,
+        b,
+        result: BlsScalar::zero(),
+        bits: 0,
+    };
+    check_satisfied_circuit(&prover, &verifier, &pi, &circuit, rng, &msg);
 
-    // negative works
-    {
-        let bits = 256;
+    // Test with bits = 32
+    //
+    // Create new prover and verifier circuit descriptions
+    let bits = 32;
+    let a = BlsScalar::zero();
+    let b = BlsScalar::zero();
+    let circuit = TestCircuit::new(a, b, bits);
+    let (prover, verifier) = setup(capacity, rng, label, &circuit);
 
-        let x = BlsScalar::pow_of_2(bits as u64) - BlsScalar::one();
+    // Test sanity:
+    let a = BlsScalar::from(0x0f0f_0ff0_0f0f_0ff0);
+    let b = BlsScalar::from(0xffff_0000_0000_ffff);
+    let result = BlsScalar::from(0x0f0f_f00f);
+    let circuit = TestCircuit { a, b, result, bits };
+    check_satisfied_circuit(&prover, &verifier, &pi, &circuit, rng, &msg);
 
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
+    // Test random works:
+    let a = BlsScalar::random(rng);
+    let b = BlsScalar::random(rng);
+    let circuit = TestCircuit::new(a, b, bits);
+    check_satisfied_circuit(&prover, &verifier, &pi, &circuit, rng, &msg);
 
-        let a = a & x;
-        let b = b & x;
-        let c = (a ^ b) & x;
+    // Test invalid circuit fails
+    let msg = "Proof creation of unsatisfied circuit should fail";
+    let bit_mask = BlsScalar::pow_of_2(bits as u64) - BlsScalar::one();
+    let a = BlsScalar::random(rng);
+    let b = BlsScalar::random(rng);
+    let right_result = (a ^ b) & bit_mask;
+    let c = BlsScalar::random(rng);
+    let wrong_result = (a ^ c) & bit_mask;
+    assert_ne!(right_result, wrong_result);
+    let circuit_unsatisfied = TestCircuit {
+        a,
+        b,
+        result: wrong_result,
+        bits,
+    };
+    check_unsatisfied_circuit(&prover, &circuit_unsatisfied, rng, &msg);
+    // sanity check
+    let circuit_satisfied = TestCircuit {
+        a,
+        b,
+        result: right_result,
+        bits,
+    };
+    check_satisfied_circuit(
+        &prover,
+        &verifier,
+        &pi,
+        &circuit_satisfied,
+        rng,
+        &"Sanity check should pass",
+    );
 
-        let m = BlsScalar::random(rng) & x;
-        let n = (a ^ m) & x;
+    // Test with bits = 256
+    //
+    // Create new prover and verifier circuit descriptions
+    let bits = 256;
+    let a = BlsScalar::zero();
+    let b = BlsScalar::zero();
+    let circuit = TestCircuit::new(a, b, bits);
+    let (prover, verifier) = setup(capacity, rng, label, &circuit);
 
-        assert_ne!(c, n);
+    // Test sanity:
+    let a = -BlsScalar::one();
+    let b = BlsScalar::zero();
+    let result = -BlsScalar::one();
+    let circuit = TestCircuit { a, b, result, bits };
+    check_satisfied_circuit(&prover, &verifier, &pi, &circuit, rng, &msg);
 
-        prover
-            .prove(rng, &DummyCircuit { a, b, c: n, bits })
-            .expect_err("the provided proof isn't valid");
-    }
+    // Test random works:
+    let msg = "Circuit verification with random values should pass";
+    let a = BlsScalar::random(rng);
+    let b = BlsScalar::random(rng);
+    let circuit = TestCircuit::new(a, b, bits);
+    check_satisfied_circuit(&prover, &verifier, &pi, &circuit, rng, &msg);
 
-    // small bits works
-    {
-        let bits = 30;
+    // Test invalid circuit fails
+    let msg = "Proof creation of unsatisfied circuit should fail";
+    let a = BlsScalar::random(rng);
+    let b = BlsScalar::random(rng);
+    let right_result = a ^ b;
+    let c = BlsScalar::random(rng);
+    let wrong_result = a ^ c;
+    assert_ne!(right_result, wrong_result);
+    let circuit = TestCircuit {
+        a,
+        b,
+        result: wrong_result,
+        bits,
+    };
+    check_unsatisfied_circuit(&prover, &circuit, rng, &msg);
+    // sanity check
+    let circuit_satisfied = TestCircuit {
+        a,
+        b,
+        result: right_result,
+        bits,
+    };
+    check_satisfied_circuit(
+        &prover,
+        &verifier,
+        &pi,
+        &circuit_satisfied,
+        rng,
+        &"Sanity check should pass",
+    );
 
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
+    // Test with odd bits = 55
+    //
+    // Create new prover and verifier circuit descriptions
+    let bits = 55;
+    let a = BlsScalar::zero();
+    let b = BlsScalar::zero();
+    let circuit = TestCircuit::new(a, b, bits);
+    let (prover, _) = setup(capacity, rng, label, &circuit);
 
-        let circuit = DummyCircuit::new(a, b, bits);
-
-        let (prover, verifier) =
-            Compiler::compile_with_circuit(&pp, label, &circuit)
-                .expect("failed to compile circuit");
-
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
-
-        let (proof, public_inputs) = prover
-            .prove(rng, &DummyCircuit::new(a, b, bits))
-            .expect("failed to prove");
-
-        verifier
-            .verify(&proof, &public_inputs)
-            .expect("failed to verify proof");
-    }
-
-    // zero bits works
-    {
-        let bits = 0;
-
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
-
-        let circuit = DummyCircuit::new(a, b, bits);
-
-        let (prover, verifier) =
-            Compiler::compile_with_circuit(&pp, label, &circuit)
-                .expect("failed to compile circuit");
-
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
-
-        let (proof, public_inputs) = prover
-            .prove(rng, &DummyCircuit::new(a, b, bits))
-            .expect("failed to prove");
-
-        verifier
-            .verify(&proof, &public_inputs)
-            .expect("failed to verify proof");
-    }
-
-    // odd bits will compile
-    {
-        let bits = 55;
-
-        let a = BlsScalar::random(rng);
-        let b = BlsScalar::random(rng);
-
-        let circuit = DummyCircuit::new(a, b, bits);
-
-        Compiler::compile_with_circuit(&pp, label, &circuit)
-            .expect("failed to compile circuit");
-    }
+    // Test random fails:
+    let msg = "Circuit verification with odd bits should fail";
+    let a = BlsScalar::random(rng);
+    let b = BlsScalar::random(rng);
+    let circuit = TestCircuit::new(a, b, bits);
+    check_unsatisfied_circuit(&prover, &circuit, rng, &msg);
 }
