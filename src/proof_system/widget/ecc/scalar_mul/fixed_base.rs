df5f0bd7b81e75d6a5bc33f9df2b7c4eaefe374d
Noisy <125606576+donatik27@users.noreply.github.com>
2024-11-28 16:58:53 +0100

    Update fixed_base.rs

--- src/proof_system/widget/ecc/scalar_mul/fixed_base.rs
+++ src/proof_system/widget/ecc/scalar_mul/fixed_base.rs
@@ -15,7 +15,7 @@ pub(crate) use proverkey::ProverKey;
 pub(crate) use verifierkey::VerifierKey;
 
 // Note: The ECC gadget does not check that the initial point is on the curve
-// for two reasons:
+// following reasons:
 // - We constrain the accumulator to start from the identity point, which the
 //   verifier knows is on the curve
 // - We are adding multiples of the generator to the accumulator which the
